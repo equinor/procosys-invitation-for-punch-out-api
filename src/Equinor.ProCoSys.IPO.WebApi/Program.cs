7defc454f85b806ba7e31389c42ad290cfa4023e
Henning Bjørgo <hbjorgo@users.noreply.github.com>
2020-09-09 12:50:59 +0200

    Using Azure App Configuration for storing configuration settings. (#3)

--- src/Equinor.ProCoSys.IPO.WebApi/Program.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Program.cs
@@ -1,6 +1,8 @@
-﻿using Equinor.ProCoSys.IPO.WebApi.Misc;
+﻿using System;
+using Azure.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Microsoft.Extensions.Hosting;
 
 namespace Equinor.ProCoSys.IPO.WebApi
@@ -17,11 +19,24 @@ namespace Equinor.ProCoSys.IPO.WebApi
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((context, config) =>
                 {
-                    var kvSettings = new KeyVaultSettings();
-                    config.Build().GetSection("KeyVault").Bind(kvSettings);
-                    if (kvSettings.Enabled)
+                    var settings = config.Build();
+                    if (settings.GetValue<bool>("UseAzureAppConfiguration"))
                     {
-                        config.AddAzureKeyVault(kvSettings.Uri, kvSettings.ClientId, kvSettings.ClientSecret);
+                        config.AddAzureAppConfiguration(options =>
+                        {
+                            options.Connect(settings["ConnectionStrings:AppConfig"])
+                                    .ConfigureKeyVault(kv =>
+                                    {
+                                        kv.SetCredential(new DefaultAzureCredential());
+                                    })
+                                    .ConfigureRefresh(options =>
+                                    {
+                                        options.Register("Sentinel", true);
+                                        options.SetCacheExpiration(TimeSpan.FromMinutes(5));
+                                    })
+                                    .Select(KeyFilter.Any, LabelFilter.Null)
+                                    .Select(KeyFilter.Any, context.HostingEnvironment.EnvironmentName);
+                        });
                     }
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
