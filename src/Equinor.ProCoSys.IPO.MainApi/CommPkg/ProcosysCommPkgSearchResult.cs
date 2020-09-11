1d4aaf0bd55ba4ebe28da8b2eaf313b88b03c979
Lykke Larsen <norl@statoil.com>
2020-09-11 15:44:29 +0200

    endpoint

---
+++ src/Equinor.ProCoSys.IPO.MainApi/CommPkg/ProcosysCommPkgSearchResult.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Equinor.ProCoSys.IPO.MainApi.CommPkg
+{
+    public class ProcosysCommPkgSearchResult
+    {
+        public List<ProcosysCommPkg> Items { get; set; }
+    }
+}
---
+++ src/Equinor.ProCoSys.IPO.Query/GetCommPkgsInProject/GetCommPkgsInProjectQuery.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using ServiceResult;
+
+namespace Equinor.ProCoSys.IPO.Query.GetCommPkgsInProject
+{
+    public class GetCommPkgsInProjectQuery : IRequest<Result<List<ProcosysCommPkgDto>>>
+    {
+        public GetCommPkgsInProjectQuery(int projectId, string startsWithCommPkgNo)
+        {
+            ProjectId = projectId;
+            StartsWithCommPkgNo = startsWithCommPkgNo;
+        }
+
+        public int ProjectId { get; }
+        public string StartsWithCommPkgNo { get; }
+    }
+}
