4915880f62b1ca8e573d0214bb0b396863ecc947
Lykke Larsen <norl@statoil.com>
2020-09-15 07:40:08 +0200

    typo

--- src/Equinor.ProCoSys.IPO.MainApi/CommPkg/ICommPkgApiService.cs
+++ src/Equinor.ProCoSys.IPO.MainApi/CommPkg/ICommPkgApiService.cs
@@ -5,6 +5,6 @@ namespace Equinor.ProCoSys.IPO.MainApi.CommPkg
 {
     public interface ICommPkgApiService
     {
-        Task<IList<ProCoSysCommPkg>> SearchCommPkgsByCommPkgNoAsync(string plant, int projectId, string startsWithTagNo);
+        Task<IList<ProCoSysCommPkg>> SearchCommPkgsByCommPkgNoAsync(string plant, int projectId, string startsWithCommPkgNo);
     }
 }
