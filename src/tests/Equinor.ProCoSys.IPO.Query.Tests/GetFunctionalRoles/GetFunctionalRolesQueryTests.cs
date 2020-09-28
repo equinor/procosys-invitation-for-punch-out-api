605a498da6994fa6e7ffe8d5061deda3a50a1c8c
Nora Lykke Strøm Larsen <NORL@equinor.com>
2020-09-28 15:39:10 +0200

    tests

---
+++ src/tests/Equinor.ProCoSys.IPO.Query.Tests/GetFunctionalRoles/GetFunctionalRolesQueryTests.cs
@@ -0,0 +1,18 @@
+﻿using Equinor.ProCoSys.IPO.Query.GetFunctionalRoles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.ProCoSys.IPO.Query.Tests.GetFunctionalRoles
+{
+    [TestClass]
+    public class GetFunctionalRolesQueryTests
+    {
+        [TestMethod]
+        public void Constructor_SetsProperties()
+        {
+            const string Classification = "classification";
+            var dut = new GetFunctionalRolesQuery(Classification);
+
+            Assert.AreEqual(Classification, dut.Classification);
+        }
+    }
+}
