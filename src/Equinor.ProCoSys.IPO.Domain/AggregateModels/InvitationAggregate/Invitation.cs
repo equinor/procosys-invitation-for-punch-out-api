b055df5fddddda2f0dc2c8d1158c59187e338bd8
Henning Bjørgo <HHBJ@equinor.com>
2020-09-28 10:34:42 +0200

    Div.

---
+++ src/Equinor.ProCoSys.IPO.Domain/AggregateModels/InvitationAggregate/Invitation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Equinor.ProCoSys.IPO.Domain.AggregateModels.PersonAggregate;
+using Equinor.ProCoSys.IPO.Domain.Audit;
+
+namespace Equinor.ProCoSys.IPO.Domain.AggregateModels.InvitationAggregate
+{
+    public class Invitation : PlantEntityBase, IAggregateRoot, ICreationAuditable, IModificationAuditable
+    {
+        private Invitation()
+        {
+        }
+
+        public Invitation()
+        {
+
+        }
+
+        public DateTime CreatedAtUtc => throw new NotImplementedException();
+
+        public int CreatedById => throw new NotImplementedException();
+
+        public DateTime? ModifiedAtUtc => throw new NotImplementedException();
+
+        public int? ModifiedById => throw new NotImplementedException();
+
+        public void SetCreated(Person createdBy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetModified(Person modifiedBy)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
