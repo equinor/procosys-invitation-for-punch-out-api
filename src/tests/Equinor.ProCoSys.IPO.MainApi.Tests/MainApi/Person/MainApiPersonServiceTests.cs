c111e8c85da71304fbc917b6414e977aa5e2760a
Nora Lykke Strøm Larsen <NORL@equinor.com>
2020-10-13 13:26:11 +0200

    get persons by user groups tests

---
+++ src/tests/Equinor.ProCoSys.IPO.MainApi.Tests/MainApi/Person/MainApiPersonServiceTests.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.IPO.ForeignApi.Client;
+using Equinor.ProCoSys.IPO.ForeignApi.MainApi;
+using Equinor.ProCoSys.IPO.ForeignApi.MainApi.Person;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.ProCoSys.IPO.ForeignApi.Tests.MainApi.Person
+{
+    [TestClass]
+    public class MainApiPersonServiceTests
+    {
+        private Mock<IOptionsMonitor<MainApiOptions>> _mainApiOptions;
+        private Mock<IBearerTokenApiClient> _foreignApiClient;
+        private MainApiPersonService _dut;
+        private ProCoSysPerson _proCoSysPerson1;
+        private ProCoSysPerson _proCoSysPerson2;
+        private ProCoSysPerson _proCoSysPerson3;
+
+        private const string _plant = "PCS$TESTPLANT";
+        private const string _searchString = "A";
+        private const string _userGroup = "MC_ENGINEER";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mainApiOptions = new Mock<IOptionsMonitor<MainApiOptions>>();
+            _mainApiOptions
+                .Setup(x => x.CurrentValue)
+                .Returns(new MainApiOptions { ApiVersion = "4.0", BaseAddress = "http://example.com" });
+
+            _foreignApiClient = new Mock<IBearerTokenApiClient>();
+
+            _proCoSysPerson1 = new ProCoSysPerson {
+                AzureOid = "12345678-1234-123456789123",
+                FirstName = "F1",
+                LastName = "L1",
+                UserName = "U1",
+                Email = "E1"
+            };
+            _proCoSysPerson2 = new ProCoSysPerson {
+                AzureOid = "12345678-1235-123456789123",
+                FirstName = "F2",
+                LastName = "L2",
+                UserName = "U2",
+                Email = "E2"
+            };
+            _proCoSysPerson3 = new ProCoSysPerson {
+                AzureOid = "12345678-1236-123456789123",
+                FirstName = "F3",
+                LastName = "L3",
+                UserName = "U3",
+                Email = "E3"
+            };
+
+            _foreignApiClient
+                .SetupSequence(x => x.QueryAndDeserializeAsync<List<ProCoSysPerson>>(It.IsAny<string>(), null))
+                .Returns(Task.FromResult(new List<ProCoSysPerson> { _proCoSysPerson1, _proCoSysPerson2, _proCoSysPerson3 }));
+
+            _dut = new MainApiPersonService(_foreignApiClient.Object, _mainApiOptions.Object);
+        }
+
+        [TestMethod]
+        public async Task GetPersonsByUserGroup_ShouldReturnCorrectNumberOfPersons()
+        {
+            // Act
+            var result = await _dut.GetPersonsByUserGroupAsync(_plant, _searchString, _userGroup);
+
+            // Assert
+            Assert.AreEqual(3, result.Count);
+        }
+
+        [TestMethod]
+        public async Task GetPersonsByUserGroup_ShouldReturnEmptyList_WhenResultIsInvalid()
+        {
+            _foreignApiClient
+                .Setup(x => x.QueryAndDeserializeAsync<List<ProCoSysPerson>>(It.IsAny<string>(), null))
+                .Returns(Task.FromResult(new List<ProCoSysPerson>()));
+
+            var result = await _dut.GetPersonsByUserGroupAsync(_plant, _searchString, _userGroup);
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public async Task GetPersonsByUserGroup_ShouldReturnCorrectProperties()
+        {
+            // Act
+            var result = await _dut.GetPersonsByUserGroupAsync(_plant, _searchString, _userGroup);
+
+            // Assert
+            var person = result.First();
+            Assert.AreEqual("12345678-1234-123456789123", person.AzureOid);
+            Assert.AreEqual("F1", person.FirstName);
+            Assert.AreEqual("L1", person.LastName);
+            Assert.AreEqual("U1", person.UserName);
+            Assert.AreEqual("E1", person.Email);
+        }
+    }
+}
---
+++ src/tests/Equinor.ProCoSys.IPO.Query.Tests/GetPersonsInUserGroup/GetPersonsInUserGroupQueryHandlerTests.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.IPO.ForeignApi.MainApi.Person;
+using Equinor.ProCoSys.IPO.Infrastructure;
+using Equinor.ProCoSys.IPO.Query.GetPersonsInUserGroup;
+using Equinor.ProCoSys.IPO.Test.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ServiceResult;
+
+namespace Equinor.ProCoSys.IPO.Query.Tests.GetPersonsInUserGroup
+{
+    [TestClass]
+    public class GetPersonsInUserGroupQueryHandlerTests : ReadOnlyTestsBase
+    {
+        private Mock<IPersonApiService> _personApiServiceMock;
+        private IList<ProCoSysPerson> _mainApiContractorPersons;
+        private IList<ProCoSysPerson> _mainApiConstructionPersons;
+        private GetPersonsInUserGroupQuery _query;
+
+        private readonly string _contractorUserGroup = "MC_CONTRACTOR_MLA";
+        private readonly string _constructionUserGroup = "MC_LEAD_DISCIPLINE";
+        private readonly string _searchString = "A";
+
+        protected override void SetupNewDatabase(DbContextOptions<IPOContext> dbContextOptions)
+        {
+            using (new IPOContext(dbContextOptions, _plantProvider, _eventDispatcher, _currentUserProvider))
+            {
+                _personApiServiceMock = new Mock<IPersonApiService>();
+                _mainApiContractorPersons = new List<ProCoSysPerson>
+                {
+                    new ProCoSysPerson
+                    {
+                        AzureOid = "12345678-1234-123456789123",
+                        FirstName = "F1",
+                        LastName = "L1",
+                        UserName = "U1",
+                        Email = "E1"
+                    },
+                    new ProCoSysPerson
+                    {
+                        AzureOid = "12345678-1235-123456789123",
+                        FirstName = "F2",
+                        LastName = "L2",
+                        UserName = "U2",
+                        Email = "E2"
+                    },
+                    new ProCoSysPerson
+                    {
+                        AzureOid = "12345678-1236-123456789123",
+                        FirstName = "F3",
+                        LastName = "L3",
+                        UserName = "U3",
+                        Email = "E3"
+                    }
+                };
+
+                _personApiServiceMock = new Mock<IPersonApiService>();
+                _mainApiConstructionPersons = new List<ProCoSysPerson>
+                {
+                    new ProCoSysPerson
+                    {
+                        AzureOid = "12345678-1237-123456789123",
+                        FirstName = "FN4",
+                        LastName = "LN4",
+                        UserName = "UN4",
+                        Email = "EM4"
+                    },
+                    new ProCoSysPerson
+                    {
+                        AzureOid = "12345678-1238-123456789123",
+                        FirstName = "FN5",
+                        LastName = "LN5",
+                        UserName = "UN5",
+                        Email = "EM5"
+                    },
+                    new ProCoSysPerson
+                    {
+                        AzureOid = "12345678-1239-123456789123",
+                        FirstName = "FN6",
+                        LastName = "LN6",
+                        UserName = "UN6",
+                        Email = "EM6"
+                    }
+                };
+
+                _query = new GetPersonsInUserGroupQuery(_searchString, _contractorUserGroup);
+            }
+        }
+
+        [TestMethod]
+        public async Task Handle_ShouldReturnOkResult()
+        {
+            using (new IPOContext(_dbContextOptions, _plantProvider, _eventDispatcher, _currentUserProvider))
+            {
+                var dut = new GetPersonsInUserGroupQueryHandler(_personApiServiceMock.Object, _plantProvider);
+                var result = await dut.Handle(_query, default);
+
+                Assert.AreEqual(ResultType.Ok, result.ResultType);
+            }
+        }
+
+        [TestMethod]
+        public async Task HandleGetContractorPersons_ShouldReturnCorrectItems()
+        {
+            using (new IPOContext(_dbContextOptions, _plantProvider, _eventDispatcher, _currentUserProvider))
+            {
+                _personApiServiceMock
+                    .Setup(x => x.GetPersonsByUserGroupAsync(TestPlant, _searchString, _contractorUserGroup))
+                    .Returns(Task.FromResult(_mainApiContractorPersons));
+
+                _query = new GetPersonsInUserGroupQuery(_searchString, _contractorUserGroup);
+
+                var dut = new GetPersonsInUserGroupQueryHandler(_personApiServiceMock.Object, _plantProvider);
+                var result = await dut.Handle(_query, default);
+
+                Assert.AreEqual(3, result.Data.Count);
+                var person1 = result.Data.ElementAt(0);
+                var person2 = result.Data.ElementAt(1);
+                var person3 = result.Data.ElementAt(2);
+                AssertPersonData(_mainApiContractorPersons.Single(c => c.AzureOid == person1.Oid), person1);
+                AssertPersonData(_mainApiContractorPersons.Single(t => t.AzureOid == person2.Oid), person2);
+                AssertPersonData(_mainApiContractorPersons.Single(t => t.AzureOid == person3.Oid), person3);
+            }
+        }
+
+        [TestMethod]
+        public async Task HandleGetContractorPersons_ShouldReturnEmptyList_WhenSearchReturnsNull()
+        {
+            using (new IPOContext(_dbContextOptions, _plantProvider, _eventDispatcher, _currentUserProvider))
+            {
+                _query = new GetPersonsInUserGroupQuery(_searchString, _contractorUserGroup);
+
+                var dut = new GetPersonsInUserGroupQueryHandler(_personApiServiceMock.Object, _plantProvider);
+                _personApiServiceMock
+                    .Setup(x => x.GetPersonsByUserGroupAsync(TestPlant, _searchString, _contractorUserGroup))
+                    .Returns(Task.FromResult<IList<ProCoSysPerson>>(null));
+
+                var result = await dut.Handle(_query, default);
+
+                Assert.AreEqual(ResultType.Ok, result.ResultType);
+                Assert.AreEqual(0, result.Data.Count);
+            }
+        }
+
+        [TestMethod]
+        public async Task HandleGetConstructionPersons_ShouldReturnCorrectItems()
+        {
+            using (new IPOContext(_dbContextOptions, _plantProvider, _eventDispatcher, _currentUserProvider))
+            {
+                _personApiServiceMock
+                    .Setup(x => x.GetPersonsByUserGroupAsync(TestPlant, _searchString, _constructionUserGroup))
+                    .Returns(Task.FromResult(_mainApiConstructionPersons));
+
+                _query = new GetPersonsInUserGroupQuery(_searchString, _constructionUserGroup);
+
+                var dut = new GetPersonsInUserGroupQueryHandler(_personApiServiceMock.Object, _plantProvider);
+                var result = await dut.Handle(_query, default);
+
+                Assert.AreEqual(3, result.Data.Count);
+                var person1 = result.Data.ElementAt(0);
+                var person2 = result.Data.ElementAt(1);
+                var person3 = result.Data.ElementAt(2);
+                AssertPersonData(_mainApiConstructionPersons.Single(c => c.AzureOid == person1.Oid), person1);
+                AssertPersonData(_mainApiConstructionPersons.Single(t => t.AzureOid == person2.Oid), person2);
+                AssertPersonData(_mainApiConstructionPersons.Single(t => t.AzureOid == person3.Oid), person3);
+            }
+        }
+
+        [TestMethod]
+        public async Task HandleGetConstructionPersons_ShouldReturnEmptyList_WhenSearchReturnsNull()
+        {
+            _query = new GetPersonsInUserGroupQuery(_searchString, _constructionUserGroup);
+
+            using (new IPOContext(_dbContextOptions, _plantProvider, _eventDispatcher, _currentUserProvider))
+            {
+                var dut = new GetPersonsInUserGroupQueryHandler(_personApiServiceMock.Object, _plantProvider);
+                _personApiServiceMock
+                    .Setup(x => x.GetPersonsByUserGroupAsync(TestPlant, _searchString, _constructionUserGroup))
+                    .Returns(Task.FromResult<IList<ProCoSysPerson>>(null));
+
+                var result = await dut.Handle(_query, default);
+
+                Assert.AreEqual(ResultType.Ok, result.ResultType);
+                Assert.AreEqual(0, result.Data.Count);
+            }
+        }
+
+        private void AssertPersonData(ProCoSysPerson PCSPerson, ProCoSysPersonDto personDto)
+        {
+            Assert.AreEqual(PCSPerson.AzureOid, personDto.Oid);
+            Assert.AreEqual(PCSPerson.FirstName, personDto.FirstName);
+            Assert.AreEqual(PCSPerson.LastName, personDto.LastName);
+            Assert.AreEqual(PCSPerson.UserName, personDto.UserName);
+            Assert.AreEqual(PCSPerson.Email, personDto.Email);
+        }
+    }
+}
