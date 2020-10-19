dba2f4ea9ba8b216c7de0c60a4e0ada9be1b23f1
Elisabeth Bratli (ELIBRA) <ELIBRA@equinor.com>
2020-10-19 13:31:55 +0200

    moved validators

--- src/Equinor.ProCoSys.IPO.Command/InvitationCommands/CreateInvitation/CreateInvitationCommandValidator.cs
+++ src/Equinor.ProCoSys.IPO.Command/InvitationCommands/CreateInvitation/CreateInvitationCommandValidator.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Equinor.ProCoSys.IPO.Command.Validators.InvitationValidators;
+using Equinor.ProCoSys.IPO.Domain.AggregateModels.InvitationAggregate;
 using FluentValidation;
 
 namespace Equinor.ProCoSys.IPO.Command.InvitationCommands.CreateInvitation
@@ -13,19 +14,36 @@ namespace Equinor.ProCoSys.IPO.Command.InvitationCommands.CreateInvitation
             CascadeMode = CascadeMode.StopOnFirstFailure;
 
             RuleFor(command => command)
+                .Must((command) => command.ProjectName.Length > 2)
+                .WithMessage(command =>
+                    $"Project name must be at least 3 characters! ProjectName={command.ProjectName}")
+                .Must((command) => command.Description == null || command.Description.Length < 4000)
+                .WithMessage(command =>
+                    $"Description cannot be more than 4000 characters! Description={command.Description}")
+                .Must((command) => command.StartTime < command.EndTime)
+                .WithMessage(command =>
+                    $"Start time must be before end time! Start={command.StartTime} End={command.EndTime}")
+                .Must((command) => command.Title.Length > 2 && command.Title.Length < Invitation.TitleMaxLength)
+                .WithMessage(command =>
+                    $"Title must be between 3 and 1024 characters! Title={command.Title}")
+                .Must((command) => command.Description == null || command.Location.Length < 1024)
+                .WithMessage(command =>
+                    $"Location cannot be more than 1024 characters! Location={command.Location}");
+
+            RuleFor(command => command)
                 .MustAsync((command, token) => TitleMustBeUniqueOnProject(command.ProjectName, command.Title, token))
                 .WithMessage(command =>
                     $"IPO with this title already exists in project! Title={command.Title}")
-                .Must((command, token) => MustHaveValidScope(command.McPkgScope, command.CommPkgScope))
+                .Must((command) => MustHaveValidScope(command.McPkgScope, command.CommPkgScope))
                 .WithMessage(command =>
                     $"Not a valid scope! Choose either mc scope or comm pkg scope")
-                .Must((command, token) => TwoFirstParticipantsMustBeSetWithCorrectOrganization(command.Participants))
+                .Must((command) => TwoFirstParticipantsMustBeSetWithCorrectOrganization(command.Participants))
                 .WithMessage(command =>
                     $"Contractor and Construction Company must be invited!")
-                .Must((command, token) => RequiredParticipantsHaveLowestSortKeys(command.Participants))
+                .Must((command) => RequiredParticipantsHaveLowestSortKeys(command.Participants))
                 .WithMessage(command =>
                     $"SortKey 0 is reserved for Contractor, and SortKey 1 is reserved for Construction Company!")
-                .Must((command, token) => ParticipantListMustBeValid(command.Participants))
+                .Must((command) => ParticipantListMustBeValid(command.Participants))
                 .WithMessage(command =>
                     $"Each participant must contain an email or oid!");
 
