e23036df8d20ee1dfb17774c6df702e8c1f68f7b
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2020-10-21 08:59:59 +0200

    78935 add logging in middleware (#21)
    * Added more logging as in Preservation
    
    * update tests

--- src/Equinor.ProCoSys.IPO.WebApi/Middleware/CurrentUserMiddleware.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Middleware/CurrentUserMiddleware.cs
@@ -2,6 +2,7 @@
 using Equinor.ProCoSys.IPO.WebApi.Authorizations;
 using Equinor.ProCoSys.IPO.WebApi.Misc;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace Equinor.ProCoSys.IPO.WebApi.Middleware
 {
@@ -11,14 +12,20 @@ namespace Equinor.ProCoSys.IPO.WebApi.Middleware
 
         public CurrentUserMiddleware(RequestDelegate next) => _next = next;
 
-        public async Task InvokeAsync(HttpContext context, IHttpContextAccessor httpContextAccessor, ICurrentUserSetter currentUserSetter)
+        public async Task InvokeAsync(
+            HttpContext context,
+            IHttpContextAccessor httpContextAccessor,
+            ICurrentUserSetter currentUserSetter,
+            ILogger<CurrentUserMiddleware> logger)
         {
+            logger.LogInformation($"----- {GetType().Name} start");
             var oid = httpContextAccessor.HttpContext.User.Claims.TryGetOid();
             if (oid.HasValue)
             {
                 currentUserSetter.SetCurrentUser(oid.Value);
             }
 
+            logger.LogInformation($"----- {GetType().Name} complete");
             // Call the next delegate/middleware in the pipeline
             await _next(context);
         }
