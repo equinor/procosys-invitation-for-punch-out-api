493180839214eb811736b1fc23a21bcff5e0ca7e
Henning Bjørgo <HHBJ@equinor.com>
2020-10-22 08:27:34 +0200

    Filtering.

--- src/Equinor.ProCoSys.IPO.BlobStorage/BlobStorageOptions.cs
+++ src/Equinor.ProCoSys.IPO.BlobStorage/BlobStorageOptions.cs
@@ -6,6 +6,6 @@
         public int MaxSizeMb { get; set; }
         public string BlobContainer { get; set; }
         public int BlobClockSkewMinutes { get; set; }
-        public string[] ValidFileSuffixes { get; set; }
+        public string[] BlockedFileSuffixes { get; set; }
     }
 }
---
+++ src/Equinor.ProCoSys.IPO.WebApi/Controllers/Invitation/DeleteAttachmentDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Equinor.ProCoSys.IPO.WebApi.Controllers.Invitation
+{
+    public class DeleteAttachmentDtoValidator : AbstractValidator<DeleteAttachmentDto>
+    {
+        public DeleteAttachmentDtoValidator()
+        {
+            RuleFor(x => x.RowVersion)
+                .NotEmpty();
+        }
+    }
+}
