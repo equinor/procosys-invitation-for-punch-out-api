efce3a9ebe286d11ecf98c62045665dc34d46789
Henning Bjørgo <HHBJ@equinor.com>
2020-10-22 09:35:08 +0200

    Added migration

---
+++ src/Equinor.ProCoSys.IPO.Infrastructure/Migrations/20201022073138_Attachments.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.ProCoSys.IPO.Infrastructure.Migrations
+{
+    public partial class Attachments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Attachment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Plant = table.Column<string>(maxLength: 255, nullable: false),
+                    BlobStorageId = table.Column<Guid>(nullable: false),
+                    FileName = table.Column<string>(maxLength: 255, nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(nullable: false),
+                    CreatedById = table.Column<int>(nullable: false),
+                    ModifiedAtUtc = table.Column<DateTime>(nullable: true),
+                    ModifiedById = table.Column<int>(nullable: true),
+                    InvitationId = table.Column<int>(nullable: true),
+                    RowVersion = table.Column<byte[]>(rowVersion: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attachment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Attachment_Persons_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "Persons",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Attachment_Invitations_InvitationId",
+                        column: x => x.InvitationId,
+                        principalTable: "Invitations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attachment_CreatedById",
+                table: "Attachment",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attachment_InvitationId",
+                table: "Attachment",
+                column: "InvitationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attachment");
+        }
+    }
+}
