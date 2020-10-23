4a85654e491692a19c8d7d2e24bc5b272062a6b5
Henning Bjørgo <HHBJ@equinor.com>
2020-10-23 12:03:11 +0200

    Fix

--- src/tests/Equinor.ProCoSys.IPO.Command.Tests/InvitationCommands/UploadAttachment/UploadAttachmentCommandValidatorTests.cs
+++ src/tests/Equinor.ProCoSys.IPO.Command.Tests/InvitationCommands/UploadAttachment/UploadAttachmentCommandValidatorTests.cs
@@ -1,8 +1,10 @@
 ﻿using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using Equinor.ProCoSys.IPO.BlobStorage;
 using Equinor.ProCoSys.IPO.Command.InvitationCommands.UploadAttachment;
 using Equinor.ProCoSys.IPO.Command.Validators.InvitationValidators;
+using Microsoft.Extensions.Options;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -12,6 +14,7 @@ namespace Equinor.ProCoSys.IPO.Command.Tests.InvitationCommands.UploadAttachment
     public class UploadAttachmentCommandValidatorTests
     {
         private Mock<IInvitationValidator> _validatorMock;
+        private IOptionsMonitor<BlobStorageOptions> _monitor;
         private UploadAttachmentCommandValidator _dut;
 
         [TestInitialize]
@@ -23,7 +26,14 @@ namespace Equinor.ProCoSys.IPO.Command.Tests.InvitationCommands.UploadAttachment
                 .Returns(Task.FromResult(true));
             _validatorMock.Setup(x => x.AttachmentWithFileNameExistsAsync(1, "existingfile.txt", It.IsAny<CancellationToken>()))
                 .Returns(Task.FromResult(true));
-            _dut = new UploadAttachmentCommandValidator(_validatorMock.Object);
+            var blobStorageOptions = new BlobStorageOptions()
+            {
+                BlobContainer = "TestContainer",
+                MaxSizeMb = 1,
+                BlockedFileSuffixes = new[] { ".exe" }
+            };
+            _monitor = Mock.Of<IOptionsMonitor<BlobStorageOptions>>(x => x.CurrentValue == blobStorageOptions);
+            _dut = new UploadAttachmentCommandValidator(_validatorMock.Object, _monitor);
         }
 
         [TestMethod]
@@ -68,6 +78,45 @@ namespace Equinor.ProCoSys.IPO.Command.Tests.InvitationCommands.UploadAttachment
             var result = _dut.Validate(command);
 
             Assert.IsTrue(result.IsValid);
+        }
+
+        [TestMethod]
+        public void Validate_Fails_WhenFileNameIsNotGiven()
+        {
+
+            var command = new UploadAttachmentCommand(1, string.Empty, false, new MemoryStream());
+
+            var result = _dut.Validate(command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Filename not given!"));
+        }
+
+        [TestMethod]
+        public void Validate_Fails_WhenFileNameIsTooLong()
+        {
+
+            var command = new UploadAttachmentCommand(1, new string('a', 252) + ".txt", false, new MemoryStream());
+
+            var result = _dut.Validate(command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Filename too long!"));
+        }
+
+        [TestMethod]
+        public void Validate_Fails_WhenAttachmentIsTooLarge()
+        {
+            var data = new byte[2 * 1024 * 1024];
+            var command = new UploadAttachmentCommand(1, "newfile.txt", false, new MemoryStream(data));
+
+            var result = _dut.Validate(command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Maximum file size is"));
         }
     }
 }
