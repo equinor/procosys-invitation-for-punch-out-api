4883be0e04e6d621daed9114693f0cfbdd9c38fc
Nora Lykke Strøm Larsen <NORL@equinor.com>
2020-10-27 12:41:22 +0100

    add description to invitation

---
+++ src/Equinor.ProCoSys.IPO.Infrastructure/Migrations/20201027113031_AddDescriptionToInvitation.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.ProCoSys.IPO.Infrastructure.Migrations
+{
+    public partial class AddDescriptionToInvitation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Invitations",
+                maxLength: 4096,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Invitations");
+        }
+    }
+}
