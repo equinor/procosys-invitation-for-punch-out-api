28c4f8259c8fe3cbd29173e4775c49891f04dee3
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-11-09 19:16:13 +0100

    Renamed ICurrentUserSetter.SetCurrentUser to SetCurrentUserOid

--- src/Equinor.ProCoSys.IPO.Domain/AggregateModels/InvitationAggregate/Invitation.cs
+++ src/Equinor.ProCoSys.IPO.Domain/AggregateModels/InvitationAggregate/Invitation.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Equinor.ProCoSys.IPO.Domain.AggregateModels.PersonAggregate;
 using Equinor.ProCoSys.IPO.Domain.Audit;
 using Equinor.ProCoSys.IPO.Domain.Time;
