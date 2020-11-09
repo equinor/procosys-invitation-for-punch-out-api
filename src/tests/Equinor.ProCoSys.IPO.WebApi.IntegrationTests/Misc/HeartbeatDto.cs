1c2bdb869a550c9d51f534047975d13fa95827cc
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-11-09 20:05:03 +0100

    added tests for HeartbeatController

---
+++ src/tests/Equinor.ProCoSys.IPO.WebApi.IntegrationTests/Misc/HeartbeatDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Equinor.ProCoSys.IPO.WebApi.IntegrationTests.Misc
+{
+    public class HeartbeatDto
+    {
+        public bool IsAlive { get; set; }
+        public string TimeStamp { get; set; }
+    }
+}
