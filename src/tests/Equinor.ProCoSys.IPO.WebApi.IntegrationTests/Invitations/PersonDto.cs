de003240a24b029588a5753cd3426e8ec980c46b
Lykke Larsen <NORL@statoil.com>
2020-12-22 09:59:50 +0100

    not nullable azureoid

--- src/tests/Equinor.ProCoSys.IPO.WebApi.IntegrationTests/Invitations/PersonDto.cs
+++ src/tests/Equinor.ProCoSys.IPO.WebApi.IntegrationTests/Invitations/PersonDto.cs
@@ -6,7 +6,7 @@ namespace Equinor.ProCoSys.IPO.WebApi.IntegrationTests.Invitations
     { 
         public int Id { get; set; }
         public string RowVersion { get; set; }
-        public Guid? AzureOid { get; set; }
+        public Guid AzureOid { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
