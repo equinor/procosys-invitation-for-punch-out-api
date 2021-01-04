56da549d5f2d08f94cd66aea86be9614f23b8b9e
Elisabeth Bratli (ELIBRA) <ELIBRA@equinor.com>
2021-01-04 17:29:45 +0100

    tests

---
+++ src/tests/Equinor.ProCoSys.IPO.WebApi.IntegrationTests/Invitations/HistoryDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Equinor.ProCoSys.IPO.Domain.AggregateModels.HistoryAggregate;
+using Equinor.ProCoSys.IPO.Query;
+
+namespace Equinor.ProCoSys.IPO.WebApi.IntegrationTests.Invitations
+{
+    public class HistoryDto
+    {
+        public int Id { get; }
+        public string Description { get; }
+        public DateTime CreatedAtUtc { get; }
+        public PersonMinimalDto CreatedBy { get; }
+        public EventType EventType { get; }
+    }
+}
