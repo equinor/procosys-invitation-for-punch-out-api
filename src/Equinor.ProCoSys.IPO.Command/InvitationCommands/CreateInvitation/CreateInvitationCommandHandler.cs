f28b8dd8105d57f1f984267d692ad81e2c85de9a
Elisabeth Bratli <elibra@equinor.com>
2021-01-19 08:26:07 +0100

    insert necessary line that was removed in previous PR (#70)

--- src/Equinor.ProCoSys.IPO.Command/InvitationCommands/CreateInvitation/CreateInvitationCommandHandler.cs
+++ src/Equinor.ProCoSys.IPO.Command/InvitationCommands/CreateInvitation/CreateInvitationCommandHandler.cs
@@ -439,6 +439,7 @@ namespace Equinor.ProCoSys.IPO.Command.InvitationCommands.CreateInvitation
                     .WithTimeZone("UTC")
                     .WithParticipants(participants)
                     .WithClassification(MeetingClassification.Open)
+                    .EnableOutlookIntegration(OutlookMode.All) //this line is necessary!
                     .WithInviteBodyHtml(GenerateMeetingDescription(invitation));
             });
             return meeting.Id;
