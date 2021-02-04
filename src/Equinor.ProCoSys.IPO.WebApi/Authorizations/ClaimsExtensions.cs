f67d29a72c361ddfe3e6eb8601a9a9f69f13056a
Henning Bjørgo <hbjorgo@users.noreply.github.com>
2021-02-04 10:46:57 +0100

    Adding checks to avoid arguments out of range. (#90)
    * Adding checks to avoid arguments out of range.
    
    * Added unit tests and made some fixes.
    
    * Added unit tests.

--- src/Equinor.ProCoSys.IPO.WebApi/Authorizations/ClaimsExtensions.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Authorizations/ClaimsExtensions.cs
@@ -34,7 +34,17 @@ namespace Equinor.ProCoSys.IPO.WebApi.Authorizations
             claim = claims.SingleOrDefault(c => c.Type == Name);
             if (claim != null)
             {
-                return claim.Value.Substring(0, claim.Value.Length - (claim.Value.Length - claim.Value.LastIndexOf(' ')));
+                var length = claim.Value.Length;
+                var indexOfLastWhiteSpace = claim.Value.LastIndexOf(' ');
+                if (length > 0 && indexOfLastWhiteSpace > 0)
+                {
+                    var givenName = claim.Value.Substring(0, indexOfLastWhiteSpace);
+                    if (string.IsNullOrWhiteSpace(givenName))
+                    {
+                        return default;
+                    }
+                    return givenName;
+                }
             }
 
             return default;
@@ -49,9 +59,13 @@ namespace Equinor.ProCoSys.IPO.WebApi.Authorizations
             }
 
             claim = claims.SingleOrDefault(c => c.Type == Name);
-            if (claim != null)
+            if (claim != null && !string.IsNullOrWhiteSpace(claim.Value))
             {
-                return claim.Value.Split(' ').Last();
+                var split = claim.Value.Split(' ');
+                if (split.Length > 1)
+                {
+                    return split.Last();
+                }
             }
 
             return default;
