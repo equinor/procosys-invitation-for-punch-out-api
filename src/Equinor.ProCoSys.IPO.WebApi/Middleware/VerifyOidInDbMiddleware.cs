d4d5295acb3d0bc0c6bd291f61d193de1c1944a7
Henning Bjørgo <HHBJ@equinor.com>
2021-02-05 12:03:16 +0100

    Remove temorary logging of claims.

--- src/Equinor.ProCoSys.IPO.WebApi/Middleware/VerifyOidInDbMiddleware.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Middleware/VerifyOidInDbMiddleware.cs
@@ -33,17 +33,6 @@ namespace Equinor.ProCoSys.IPO.WebApi.Middleware
                 var userName = httpContextUser.Claims.TryGetUserName();
                 var email = httpContextUser.Claims.TryGetEmail();
 
-                if (givenName == null || surName == null || userName == null || email == null)
-                {
-                    var givenNameClaim = httpContextUser.Claims.SingleOrDefault(x => x.Type == ClaimTypes.GivenName);
-                    var surNameclaim = httpContextUser.Claims.SingleOrDefault(c => c.Type == ClaimTypes.Surname);
-                    var nameClaim = httpContextUser.Claims.SingleOrDefault(c => c.Type == ClaimsExtensions.Name);
-                    var upnClaim = httpContextUser.Claims.SingleOrDefault(x => x.Type == ClaimTypes.Upn);
-                    var unique_nameClaim = httpContextUser.Claims.SingleOrDefault(x => x.Type == ClaimsExtensions.UniqueName);
-                    var unique_nameRenamedClaim = httpContextUser.Claims.SingleOrDefault(x => x.Type == ClaimTypes.Name);
-                    logger.LogError($"Claim(s) not found. GivenName found: {givenNameClaim != null}, GivenName value: {givenNameClaim?.Value}. SurName found: {surNameclaim != null}, SurName value: {surNameclaim?.Value}. Name found: {nameClaim != null}, Name value: {nameClaim?.Value}.  UPN found: {upnClaim != null}, UPN value: {upnClaim?.Value}. Unique_name found: {unique_nameClaim != null}, Unique_name value: {unique_nameClaim?.Value}. Unique_name_renamed found: {unique_nameRenamedClaim != null}, Unique_name_renamed value: {unique_nameRenamedClaim?.Value}.");
-                }
-
                 var command = new CreatePersonCommand(oid.Value, givenName, surName, userName, email);
                 try
                 {
