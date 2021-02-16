8c48f58b5e294033567eca141496038e266ed46c
janhagevold <54887770+janhagevold@users.noreply.github.com>
2021-02-16 06:50:20 +0100

    Bug 80609: Text fields too big for outlook (#79)
    * Change title and location max length
    
    * Fix tests
    
    * Add migration script for max length title and location

---
+++ src/Equinor.ProCoSys.IPO.Infrastructure/Migrations/20210215124321_ChangeMaxLengthLocationTitle.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.ProCoSys.IPO.Infrastructure.Migrations
+{
+    public partial class ChangeMaxLengthLocationTitle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE INVITATIONS SET TITLE=SUBSTRING(TITLE, 1, 250)");
+            migrationBuilder.Sql("UPDATE INVITATIONS SET LOCATION=SUBSTRING(LOCATION, 1, 250)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Invitations",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1024)",
+                oldMaxLength: 1024);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Invitations",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1024)",
+                oldMaxLength: 1024,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Invitations",
+                type: "nvarchar(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Invitations",
+                type: "nvarchar(1024)",
+                maxLength: 1024,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250,
+                oldNullable: true);
+        }
+    }
+}
