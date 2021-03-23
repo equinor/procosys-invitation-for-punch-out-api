2ab1d1c383829c6c2a9a09dfb17ab6922ab9562c
Elisabeth Bratli (ELIBRA) <ELIBRA@equinor.com>
2021-03-23 10:49:25 +0100

    Add indexes on properties used in filter

---
+++ src/Equinor.ProCoSys.IPO.Infrastructure/Migrations/20210323093442_AddIndexes.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.ProCoSys.IPO.Infrastructure.Migrations
+{
+    public partial class AddIndexes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Invitations_ProjectName",
+                table: "Invitations",
+                column: "ProjectName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invitations_Title",
+                table: "Invitations",
+                column: "Title");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invitations_Status",
+                table: "Invitations",
+                column: "Status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommPkgs_CommPkgNo",
+                table: "CommPkgs",
+                column: "CommPkgNo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_McPkgs_McPkgNo",
+                table: "McPkgs",
+                column: "McPkgNo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
