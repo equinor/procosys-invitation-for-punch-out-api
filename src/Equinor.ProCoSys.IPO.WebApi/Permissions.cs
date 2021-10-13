f3d7309dff5f8435b0f8da0c4977ca24631b23d7
Jan Inge Dalsbø <JDALSB@equinor.com>
2021-10-13 08:05:27 +0200

    Minor to trigger redeploy

--- src/Equinor.ProCoSys.IPO.WebApi/Permissions.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Permissions.cs
@@ -7,7 +7,6 @@
         public const string MCPKG_READ = "MCPKG/READ";
         public const string LIBRARY_FUNCTIONAL_ROLE_READ = "LIBRARY_FUNCTIONAL_ROLE/READ";
         public const string USER_READ = "USER/READ";
-
         public const string IPO_READ = "IPO/READ";
         public const string IPO_CREATE = "IPO/CREATE";
         public const string IPO_WRITE = "IPO/WRITE";
