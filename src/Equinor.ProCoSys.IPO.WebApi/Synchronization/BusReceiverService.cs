d2f25ec9143cb4d1add5198cc518f994615c5a6d
Lykke Larsen <NORL@statoil.com>
2021-12-01 09:27:31 +0100

    update functionalrolecode on participants

--- src/Equinor.ProCoSys.IPO.WebApi/Synchronization/BusReceiverService.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Synchronization/BusReceiverService.cs
@@ -65,6 +65,9 @@ namespace Equinor.ProCoSys.IPO.WebApi.Synchronization
                 case PcsTopic.McPkg:
                     ProcessMcPkgEvent(messageJson);
                     break;
+                case PcsTopic.Library:
+                    ProcessLibraryEvent(messageJson);
+                    break;
             }
             await _unitOfWork.SaveChangesAsync(cancellationToken);
         }
@@ -221,6 +224,25 @@ namespace Equinor.ProCoSys.IPO.WebApi.Synchronization
             }
         }
 
+        private void ProcessLibraryEvent(string messageJson)
+        {
+            var libraryEvent = JsonSerializer.Deserialize<LibraryTopic>(messageJson);
+            if (string.IsNullOrWhiteSpace(libraryEvent.Plant))
+            {
+                throw new Exception($"Unable to deserialize JSON to LibraryEvent {messageJson}");
+            }
+
+            _telemetryClient.TrackEvent(IpoBusReceiverTelemetryEvent,
+                new Dictionary<string, string>
+                {
+                    {PcsServiceBusTelemetryConstants.Event, IpoTopic.TopicName},
+                    {PcsServiceBusTelemetryConstants.Plant, libraryEvent.Plant[4..]},
+                });
+            _plantSetter.SetPlant(libraryEvent.Plant);
+
+            _invitationRepository.UpdateFunctionalRoleCodesOnInvitations(libraryEvent.Plant, libraryEvent.CodeOld, libraryEvent.Code);
+        }
+
         private async Task ClearM01DatesAndBlankExternalReferenceAsync(IpoTopic ipoEvent, Invitation invitation)
         {
             try
@@ -230,7 +252,7 @@ namespace Equinor.ProCoSys.IPO.WebApi.Synchronization
                     null,
                     invitation.ProjectName,
                     invitation.McPkgs.Select(mcPkg => mcPkg.McPkgNo).ToList(),
-                    invitation.CommPkgs.Select(c => c.CommPkgNo).ToList());
+                    invitation.CommPkgs.Select(commPkg => commPkg.CommPkgNo).ToList());
             }
             catch (Exception e)
             {
@@ -247,7 +269,7 @@ namespace Equinor.ProCoSys.IPO.WebApi.Synchronization
                     invitation.Id,
                     invitation.ProjectName,
                     invitation.McPkgs.Select(mcPkg => mcPkg.McPkgNo).ToList(),
-                    invitation.CommPkgs.Select(c => c.CommPkgNo).ToList());
+                    invitation.CommPkgs.Select(commPkg => commPkg.CommPkgNo).ToList());
             }
             catch (Exception e)
             {
