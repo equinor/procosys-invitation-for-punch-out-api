8893ff54d904f85cdda095b6931e4b74a7411d56
VRiveland <vriv@equinor.com>
2021-12-19 18:14:29 +0100

    Added database migration

---
+++ src/Equinor.ProCoSys.IPO.Infrastructure/Migrations/20211217122549_AddUnsignEventType.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.ProCoSys.IPO.Infrastructure.Migrations
+{
+    public partial class AddUnsignEventType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "constraint_history_check_valid_event_type",
+                table: "History");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "constraint_history_check_valid_event_type",
+                table: "History",
+                sql: "EventType in ('IpoCompleted','IpoAccepted','IpoSigned','IpoUnsigned','IpoUncompleted','IpoUnaccepted','IpoCreated','IpoEdited','AttachmentUploaded','AttachmentRemoved','CommentAdded','CommentRemoved','IpoCanceled')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "constraint_history_check_valid_event_type",
+                table: "History");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "constraint_history_check_valid_event_type",
+                table: "History",
+                sql: "EventType in ('IpoCompleted','IpoAccepted','IpoSigned','IpoUncompleted','IpoUnaccepted','IpoCreated','IpoEdited','AttachmentUploaded','AttachmentRemoved','CommentAdded','CommentRemoved','IpoCanceled')");
+        }
+    }
+}
