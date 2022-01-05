c07cf5e3c6fc452e69254860caf746f745daec2c
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2022-01-05 15:33:56 +0100

    88613 bugfix handling optimistic concurrency hotfix2 (#170)
    * Hotfix to treat participants in UpdatedParticipants without id as Create
    
    * New test
    
    * Added test updating a participant

--- src/Equinor.ProCoSys.IPO.Command/Validators/InvitationValidators/IInvitationValidator.cs
+++ src/Equinor.ProCoSys.IPO.Command/Validators/InvitationValidators/IInvitationValidator.cs
@@ -10,7 +10,7 @@ namespace Equinor.ProCoSys.IPO.Command.Validators.InvitationValidators
     {
         bool IsValidScope(DisciplineType type, IList<string> mcPkgScope, IList<string> commPkgScope);
         Task<bool> ParticipantWithIdExistsAsync(ParticipantsForCommand participant, int invitationId, CancellationToken token);
-        Task<bool> ParticipantExistsAsync(int? id, int invitationId, CancellationToken token);
+        Task<bool> ParticipantExistsAsync(int id, int invitationId, CancellationToken token);
         bool IsValidParticipantList(IList<ParticipantsForCommand> participants);
         bool RequiredParticipantsMustBeInvited(IList<ParticipantsForCommand> participants);
         bool OnlyRequiredParticipantsHaveLowestSortKeys(IList<ParticipantsForCommand> participants);
