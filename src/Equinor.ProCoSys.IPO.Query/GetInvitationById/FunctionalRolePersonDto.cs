2588cb1c0d65c6e6f53d06290f499d82421b12f9
Jan Inge Dalsbø <JDALSB@equinor.com>
2022-01-05 12:42:09 +0100

    Intruduced FunctionalRolePersonDto which is used in list of persons under a FunctionalRole

---
+++ src/Equinor.ProCoSys.IPO.Query/GetInvitationById/FunctionalRolePersonDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Fusion.Integration.Meeting;
+
+namespace Equinor.ProCoSys.IPO.Query.GetInvitationById
+{
+    public class FunctionalRolePersonDto
+    {
+        public FunctionalRolePersonDto(
+            int id,
+            string firstName,
+            string lastName,
+            string userName,
+            Guid azureOid,
+            string email,
+            string rowVersion)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            UserName = userName;
+            AzureOid = azureOid;
+            Email = email;
+            RowVersion = rowVersion;
+        }
+
+        public OutlookResponse? Response { get; set; }
+        public int Id { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string UserName { get; }
+        public Guid AzureOid { get; }
+        public string Email { get; }
+        public bool Required { get; set; }
+        public string RowVersion { get; }
+    }
+}
