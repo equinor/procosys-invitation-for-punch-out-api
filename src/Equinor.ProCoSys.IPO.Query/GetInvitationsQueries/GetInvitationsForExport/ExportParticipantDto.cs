3bf037f741fb9ae2dcbb38cd3ac414a492f9b902
Elisabeth Bratli <ELIBRA@equinor.com>
2022-06-09 20:55:38 +0200

    Add new columns to participant page on export - attended, note, signedby, and signedat

--- src/Equinor.ProCoSys.IPO.Query/GetInvitationsQueries/GetInvitationsForExport/ExportParticipantDto.cs
+++ src/Equinor.ProCoSys.IPO.Query/GetInvitationsQueries/GetInvitationsForExport/ExportParticipantDto.cs
@@ -1,4 +1,6 @@
-﻿namespace Equinor.ProCoSys.IPO.Query.GetInvitationsQueries.GetInvitationsForExport
+﻿using System;
+
+namespace Equinor.ProCoSys.IPO.Query.GetInvitationsQueries.GetInvitationsForExport
 {
     public class ExportParticipantDto
     {
@@ -6,17 +8,32 @@
             int id, 
             string organization,
             string type,
-            string participant)
+            string participant,
+            bool attended,
+            string note,
+            DateTime? signedAtUtc,
+            string signedBy,
+            int? signedById)
         {
             Id = id;
             Organization = organization;
             Type = type;
             Participant = participant;
+            Attended = attended;
+            Note = note;
+            SignedAtUtc = signedAtUtc;
+            SignedBy = signedBy;
+            SignedById = signedById;
         }
 
         public int Id { get; }
         public string Organization { get; }
         public string Type { get; }
         public string Participant { get; }
+        public bool Attended { get; }
+        public string Note { get; }
+        public DateTime? SignedAtUtc { get; }
+        public string SignedBy { get; set; }
+        public int? SignedById { get; }
     }
 }
