619e4855cb294343ba4d101c81136c633aa5a4cb
Christian Hormazabal <78788930+chormaz@users.noreply.github.com>
2022-08-08 14:06:00 +0200

    Make projectname required for endpoints (#199)
    * ProjectName is now a required property for /Invitations and /ExportInvitationsToExcel endpoins.
    
    * Added Fluent validator for FilterDto. ProjectName field is now mandatory in swagger.
    
    * Removed unnecessary using.

---
+++ src/Equinor.ProCoSys.IPO.WebApi/Controllers/Invitation/FilterDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Equinor.ProCoSys.IPO.WebApi.Controllers.Invitation
+{
+    public class FilterDtoValidator : AbstractValidator<FilterDto>
+    {
+        public FilterDtoValidator()
+        {
+            RuleFor(x => x)
+                .NotNull();
+
+            RuleFor(x => x.ProjectName)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+}
