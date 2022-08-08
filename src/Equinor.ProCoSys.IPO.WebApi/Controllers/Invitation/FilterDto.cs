786b2e37e788c63953814433d4b57cca1f158dd4
Christian Alejandro Hormazabal Martin <chorm@equinor.com>
2022-08-08 13:31:26 +0200

    Removed unnecessary using.

--- src/Equinor.ProCoSys.IPO.WebApi/Controllers/Invitation/FilterDto.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Controllers/Invitation/FilterDto.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Equinor.ProCoSys.IPO.Domain.AggregateModels.InvitationAggregate;
 using Equinor.ProCoSys.IPO.Query.GetInvitationsQueries;
 
