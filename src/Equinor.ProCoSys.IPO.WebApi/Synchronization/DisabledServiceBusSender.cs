b60135495dbe3d77af731a6de945714766513ed8
Tord Joranger <tord_joranger@hotmail.com>
2022-11-07 15:13:14 +0100

    Update minor versions nuget packages (#220)
    * update minor versions
    
    * add default impl to disabled servicevus

--- src/Equinor.ProCoSys.IPO.WebApi/Synchronization/DisabledServiceBusSender.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Synchronization/DisabledServiceBusSender.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
 using Equinor.ProCoSys.PcsServiceBus.Sender.Interfaces;
 
 namespace Equinor.ProCoSys.IPO.WebApi.Synchronization
@@ -9,5 +10,8 @@ namespace Equinor.ProCoSys.IPO.WebApi.Synchronization
         public Task SendAsync(string topic, string jsonMessage) => Task.CompletedTask;
 
         public Task CloseAllAsync() => Task.CompletedTask;
+        public ValueTask<ServiceBusMessageBatch> CreateMessageBatchAsync(string topic) => ValueTask.FromResult<ServiceBusMessageBatch>(null);
+
+        public Task SendMessagesAsync(ServiceBusMessageBatch messageBatch, string topic) => Task.CompletedTask;
     }
 }
