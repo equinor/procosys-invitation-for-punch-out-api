caac6ff0eb6c57cd141c79f6c13fd80c7dbc7e1f
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-03-01 12:32:07 +0100

    Renamed folder for project Equinor.ProCoSys.IPO.ForeignApi

--- src/Equinor.ProCoSys.Auth/AuthModule.cs
+++ src/Equinor.ProCoSys.Auth/AuthModule.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Equinor.ProCoSys.Auth
 {
-    public static class IServiceCollectionExtensions
+    public static class AuthModule
     {
         public static IServiceCollection AddPcsAuthIntegration(this IServiceCollection services)
         {
