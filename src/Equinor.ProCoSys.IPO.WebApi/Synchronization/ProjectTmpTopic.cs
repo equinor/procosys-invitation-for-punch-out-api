f89aac22e0beceedab8b1ad54c8a2976e050f29f
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2023-03-13 14:44:12 +0100

    100990 - Use TmpTopic-classes for deserialize bus messages (#234)
    * Use TmpTopic-classes for deserialize bus messages
    
    * fix build error

---
+++ src/Equinor.ProCoSys.IPO.WebApi/Synchronization/ProjectTmpTopic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Equinor.ProCoSys.IPO.WebApi.Synchronization
+{
+    [Obsolete("Revert to use Topic-classes from Equinor.ProCoSys.PcsServiceBus after ensuring real Guids on bus.")]
+    public class ProjectTmpTopic
+    {
+        public string Plant
+        {
+            get;
+            set;
+        }
+
+        public string ProCoSysGuid
+        {
+            get;
+            set;
+        }
+
+        public string Behavior
+        {
+            get;
+            set;
+        }
+
+        public string ProjectName
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public bool IsClosed
+        {
+            get;
+            set;
+        }
+    }
+}
