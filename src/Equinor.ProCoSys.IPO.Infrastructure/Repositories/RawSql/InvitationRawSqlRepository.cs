acac27c71bd3b1a7d90653a8993400cf6fd796e8
Kristoffer Steen <KRST@equinor.com>
2023-08-04 13:48:22 +0200

    wip

---
+++ src/Equinor.ProCoSys.IPO.Infrastructure/Repositories/RawSql/InvitationRawSqlRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.IPO.Domain.AggregateModels.InvitationAggregate;
+using Microsoft.Extensions.Configuration;
+
+namespace Equinor.ProCoSys.IPO.Infrastructure.Repositories.RawSql
+{
+    public class InvitationRawSqlRepository : RawSqlRepositoryBase
+    {
+        public InvitationRawSqlRepository(IConfiguration configuration) : base(configuration)
+        {
+
+        }
+
+        public async Task<IEnumerable<OutstandingIpoDto>>
+    }
+
+    public class OutstandingIpoDto
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public Organization Organization { get; set; }
+    }
+}
---
+++ src/Equinor.ProCoSys.IPO.Infrastructure/Repositories/RawSql/RawSqlRepositoryBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Equinor.ProCoSys.IPO.Infrastructure.Repositories.RawSql
+{
+    public class RawSqlRepositoryBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public RawSqlRepositoryBase(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IEnumerable<T>> QueryAsync<T>((string queryString, DynamicParameters parameters) query, string objectId)
+        {
+            var connectionString = _configuration.GetConnectionString("IPOContext");
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                return await connection.QueryAsync<T>(query.queryString, query.parameters);
+            }
+        }
+    }
+}
