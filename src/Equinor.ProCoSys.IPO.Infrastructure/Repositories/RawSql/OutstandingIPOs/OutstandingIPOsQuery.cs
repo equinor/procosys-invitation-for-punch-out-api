4e0336c8cc04753a1e862b764721eba1dc0fadab
Kristoffer Steen <KRST@equinor.com>
2023-08-10 15:20:18 +0200

    wip: make query more readable

--- src/Equinor.ProCoSys.IPO.Infrastructure/Repositories/RawSql/OutstandingIPOs/OutstandingIPOsQuery.cs
+++ src/Equinor.ProCoSys.IPO.Infrastructure/Repositories/RawSql/OutstandingIPOs/OutstandingIPOsQuery.cs
@@ -5,21 +5,27 @@
         private static string CreateBaseQuery()
         {
             var query = @$"
-                SELECT [i].[Id], [i].[Description], [i].[Status],
-                [p].[Id] AS [ParticipantId], [p].[AzureOid], [p].[FunctionalRoleCode], [p].[Organization], [p].[SignedAtUtc], [p].[SortKey], [p].[Type] 
-                FROM [Invitations] AS [i]
-
-                INNER JOIN [Participants] AS [p] ON [i].[Id] = [p].[InvitationId] AND [p].[Plant] = @plant
-
-                INNER JOIN [Projects] AS [pro]  ON [i].[ProjectId] = [pro].[Id] AND [pro].[Plant] = @plant             
-
-                WHERE [i].[Plant] = @plant
-                AND [i].[Status]<>(3) AND [i].[Status]<>(4)
-                AND [pro].[IsClosed] = 0
+                SELECT
+                    i.Id,
+                    i.Description,
+                    i.Status,
+                    p.Id AS ParticipantId,
+                    p.AzureOid,
+                    p.FunctionalRoleCode,
+                    p.Organization,
+                    p.SignedAtUtc,
+                    p.SortKey,
+                    p.Type 
+                FROM Invitations AS i
+                    JOIN Participants AS p ON i.Id = p.InvitationId AND p.Plant = @plant
+                    JOIN Projects AS pro  ON i.ProjectId = pro.Id AND pro.Plant = @plant
+                WHERE i.Plant = @plant
+                AND i.[Status] NOT IN (3,4)
+                AND pro.IsClosed = 0
                 AND (
-                    ([p].[SignedAtUtc] IS NULL AND [p].[Organization] <> 5 AND [p].[Organization] <> 6 AND [p].[SortKey] <> 1)
+                    (p.SignedAtUtc IS NULL AND p.Organization NOT IN(5,6) AND p.SortKey <> 1)
                     OR 
-                    ([p].[SortKey] = 1 AND [i].[Status] = 1)
+                    (p.SortKey = 1 AND i.[Status] = 1)
                     )";
             return query;
         }
@@ -28,7 +34,8 @@
         {
             var query = CreateBaseQuery();
             query += @$"
-                        AND [p].[AzureOid] = @azureOid AND [p].[FunctionalRoleCode] IS NULL ORDER BY [i].[Id]";
+                        AND p.AzureOid = @azureOid AND p.FunctionalRoleCode IS NULL
+                        ORDER BY i.Id";
             return query;
         }
 
@@ -36,7 +43,8 @@
         {
             var query = CreateBaseQuery();
             query += @$" 
-                        AND [p].[Type] = 1 AND [p].[FunctionalRoleCode] IN @functionalRoleCodes ORDER BY [i].[Id]";
+                        AND p.[Type] = 1 AND p.FunctionalRoleCode IN @functionalRoleCodes
+                        ORDER BY i.Id";
             return query;
         }
     }
