a721be5eff270fbac3639489b850504537578f75
Elisabeth Bratli <ELIBRA@equinor.com>
2023-10-13 09:55:53 +0200

    started adding rfocAcceptedAt where relevant

--- src/Equinor.ProCoSys.IPO.ForeignApi/MainApi/CommPkg/ProCoSysSearchCommPkg.cs
+++ src/Equinor.ProCoSys.IPO.ForeignApi/MainApi/CommPkg/ProCoSysSearchCommPkg.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace Equinor.ProCoSys.IPO.ForeignApi.MainApi.CommPkg
 {
@@ -10,6 +11,7 @@ namespace Equinor.ProCoSys.IPO.ForeignApi.MainApi.CommPkg
         public string CommStatus { get; set; }
         public string OperationHandoverStatus { get; set; }
         public string System { get; set; }
+        public DateTime? RfocAcceptedAt { get; set; }
         public string Section
             => System.Count(s => s == '|') == 2
                 ? System.Substring(0, System.IndexOf('|'))
