38ca4be5e31b27ff2a7e9f106bc1eb5854033197
Elisabeth Bratli <ELIBRA@equinor.com>
2023-10-24 09:39:22 +0200

    Use Try since endpoint will return NotFound if certificate is voided (or doesnt exist)

--- src/Equinor.ProCoSys.IPO.Command/InvitationCommands/UpdateRfocVoidedStatus/UpdateRfocVoidedCommandHandler.cs
+++ src/Equinor.ProCoSys.IPO.Command/InvitationCommands/UpdateRfocVoidedStatus/UpdateRfocVoidedCommandHandler.cs
@@ -63,7 +63,7 @@ namespace Equinor.ProCoSys.IPO.Command.InvitationCommands.UpdateRfocVoidedStatus
                 return new SuccessResult<Unit>(Unit.Value);
             }
 
-            var certificateMcPkgsModel = await _certificateApiService.GetCertificateMcPkgsAsync(_plantProvider.Plant, request.ProCoSysGuid);
+            var certificateMcPkgsModel = await _certificateApiService.TryGetCertificateMcPkgsAsync(_plantProvider.Plant, request.ProCoSysGuid);
 
             if (certificateMcPkgsModel != null)
             {
@@ -73,7 +73,7 @@ namespace Equinor.ProCoSys.IPO.Command.InvitationCommands.UpdateRfocVoidedStatus
                 return new UnexpectedResult<Unit>(error);
             }
 
-            var certificateCommPkgsModel = await _certificateApiService.GetCertificateCommPkgsAsync(_plantProvider.Plant, request.ProCoSysGuid);
+            var certificateCommPkgsModel = await _certificateApiService.TryGetCertificateCommPkgsAsync(_plantProvider.Plant, request.ProCoSysGuid);
 
             if (certificateCommPkgsModel != null)
             {
