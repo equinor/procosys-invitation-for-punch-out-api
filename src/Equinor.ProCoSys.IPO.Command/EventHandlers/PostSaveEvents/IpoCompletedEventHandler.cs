c45f08828df1ad064194084f30a4a285281d42fe
STATOIL-NET\ARBJE <ARBJE@EQUINOR.COM>
2023-10-25 09:34:24 +0200

    107416 - Add error handling when email-sending fails

--- src/Equinor.ProCoSys.IPO.Command/EventHandlers/PostSaveEvents/IpoCompletedEventHandler.cs
+++ src/Equinor.ProCoSys.IPO.Command/EventHandlers/PostSaveEvents/IpoCompletedEventHandler.cs
@@ -1,64 +1,37 @@
-﻿using System.Linq;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
-using Equinor.ProCoSys.Common.Email;
 using Equinor.ProCoSys.IPO.Domain.Events.PostSave;
 using Equinor.ProCoSys.PcsServiceBus.Sender.Interfaces;
 using Equinor.ProCoSys.PcsServiceBus.Topics;
 using MediatR;
-using Microsoft.Extensions.Options;
 
 namespace Equinor.ProCoSys.IPO.Command.EventHandlers.PostSaveEvents
 {
     public class IpoCompletedEventHandler : INotificationHandler<IpoCompletedEvent>
     {
         private readonly IPcsBusSender _pcsBusSender;
-        private readonly IEmailService _emailService;
-        private readonly IOptionsMonitor<MeetingOptions> _meetingOptions;
 
-        public IpoCompletedEventHandler(IPcsBusSender pcsBusSender, IEmailService emailService, IOptionsMonitor<MeetingOptions> meetingOptions)
+        public IpoCompletedEventHandler(IPcsBusSender pcsBusSender)
         {
             _pcsBusSender = pcsBusSender;
-            _emailService = emailService;
-            _meetingOptions = meetingOptions;
-        } 
+        }
 
         public async Task Handle(IpoCompletedEvent notification, CancellationToken cancellationToken)
         {
             await SendBusTopicAsync(notification);
-            await SendEmailAsync(notification, cancellationToken);
         }
 
         private async Task SendBusTopicAsync(IpoCompletedEvent notification)
         {
             var eventMessage = new BusEventMessage
             {
-                Plant = notification.Plant, Event = "Completed", InvitationGuid = notification.SourceGuid
+                Plant = notification.Plant,
+                Event = "Completed",
+                InvitationGuid = notification.SourceGuid
             };
 
             await _pcsBusSender.SendAsync(IpoTopic.TopicName, JsonSerializer.Serialize(eventMessage));
-        }
-
-        private async Task SendEmailAsync(IpoCompletedEvent notification, CancellationToken cancellationToken)
-        {
-            if (notification.Emails.Count == 0)
-            {
-                return;
-            }
-
-            var baseUrl = _meetingOptions.CurrentValue.PcsBaseUrl;
-            var id = notification.Id;
-            var title = notification.Title;
-            var plantId = notification.Plant.Split('$').Last();
-
-            var subject = $"Completed notification: IPO-{id}";
-            var body =
-                $"<p>IPO-{id}: {title} has been completed and is ready for your attention to sign and accept.</p>" +
-                "<p>Click the link to review " +
-                $"<a href=\"{baseUrl}{plantId}/InvitationForPunchOut/{id}\">IPO-{id}</a>.</p>";
-
-            await _emailService.SendEmailsAsync(notification.Emails, subject, body, cancellationToken);
         }
     }
 }
--- src/Equinor.ProCoSys.IPO.Command/InvitationCommands/CompletePunchOut/CompletePunchOutCommandHandler.cs
+++ src/Equinor.ProCoSys.IPO.Command/InvitationCommands/CompletePunchOut/CompletePunchOutCommandHandler.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Equinor.ProCoSys.Common.Email;
 using Equinor.ProCoSys.Common.Misc;
 using Equinor.ProCoSys.IPO.Domain;
 using Equinor.ProCoSys.IPO.Domain.AggregateModels.InvitationAggregate;
 using Equinor.ProCoSys.IPO.Domain.AggregateModels.PersonAggregate;
 using MediatR;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using ServiceResult;
 
 namespace Equinor.ProCoSys.IPO.Command.InvitationCommands.CompletePunchOut
@@ -18,16 +21,26 @@ namespace Equinor.ProCoSys.IPO.Command.InvitationCommands.CompletePunchOut
         private readonly IUnitOfWork _unitOfWork;
         private readonly ICurrentUserProvider _currentUserProvider;
         private readonly IPersonRepository _personRepository;
+        private readonly IOptionsMonitor<MeetingOptions> _meetingOptions;
+        private readonly IEmailService _emailService;
+        private readonly ILogger<CompletePunchOutCommandHandler> _logger;
 
-        public CompletePunchOutCommandHandler(IInvitationRepository invitationRepository,
+        public CompletePunchOutCommandHandler(
+            IInvitationRepository invitationRepository,
             IUnitOfWork unitOfWork,
             ICurrentUserProvider currentUserProvider,
-            IPersonRepository personRepository)
+            IPersonRepository personRepository,
+            IOptionsMonitor<MeetingOptions> meetingOptions,
+            IEmailService emailService,
+            ILogger<CompletePunchOutCommandHandler> logger)
         {
             _invitationRepository = invitationRepository;
             _unitOfWork = unitOfWork;
             _currentUserProvider = currentUserProvider;
             _personRepository = personRepository;
+            _meetingOptions = meetingOptions;
+            _emailService = emailService;
+            _logger = logger;
         }
 
         public async Task<Result<string>> Handle(CompletePunchOutCommand request, CancellationToken cancellationToken)
@@ -35,10 +48,12 @@ namespace Equinor.ProCoSys.IPO.Command.InvitationCommands.CompletePunchOut
             var invitation = await _invitationRepository.GetByIdAsync(request.InvitationId);
             var currentUser = await _personRepository.GetByOidAsync(_currentUserProvider.GetCurrentUserOid());
             var completedAtUtc = DateTime.UtcNow;
-            var participant = invitation.Participants.SingleOrDefault(p => 
-                p.SortKey == 0 && 
-                p.Organization == Organization.Contractor && 
+            var participant = invitation.Participants.SingleOrDefault(p =>
+                p.SortKey == 0 &&
+                p.Organization == Organization.Contractor &&
                 p.AzureOid == currentUser.Guid);
+
+            var transaction = await _unitOfWork.BeginTransaction(cancellationToken);
 
             if (participant == null || participant.FunctionalRoleCode != null)
             {
@@ -57,6 +72,19 @@ namespace Equinor.ProCoSys.IPO.Command.InvitationCommands.CompletePunchOut
             invitation.SetRowVersion(request.InvitationRowVersion);
 
             await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            try
+            {
+                await SendEmailAsync(invitation, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"User with oid {_currentUserProvider.GetCurrentUserOid()} could not complete invitation {invitation.Id}. Error occured when sending email.");
+                await transaction.RollbackAsync(cancellationToken);
+                throw new IpoSendMailException();
+            }
+
+            _unitOfWork.Commit();
             return new SuccessResult<string>(invitation.RowVersion.ConvertToString());
         }
 
@@ -70,6 +98,28 @@ namespace Equinor.ProCoSys.IPO.Command.InvitationCommands.CompletePunchOut
                 ipoParticipant.Attended = participant.Attended;
                 ipoParticipant.SetRowVersion(participant.RowVersion);
             }
+        }
+
+        private async Task SendEmailAsync(Invitation invitation, CancellationToken cancellationToken)
+        {
+            var emails = invitation.GetCompleterEmails();
+
+            if (emails.Count == 0)
+            {
+                return;
+            }
+
+            var baseUrl = _meetingOptions.CurrentValue.PcsBaseUrl;
+            var id = invitation.Id;
+            var title = invitation.Title;
+            var plantId = invitation.Plant.Split('$').Last();
+            var subject = $"Completed notification: IPO-{id}";
+            var body =
+                $"<p>IPO-{id}: {title} has been completed and is ready for your attention to sign and accept.</p>" +
+                "<p>Click the link to review " +
+                $"<a href=\"{baseUrl}{plantId}/InvitationForPunchOut/{id}\">IPO-{id}</a>.</p>";
+
+            await _emailService.SendEmailsAsync(emails, subject, body, cancellationToken);
         }
     }
 }
