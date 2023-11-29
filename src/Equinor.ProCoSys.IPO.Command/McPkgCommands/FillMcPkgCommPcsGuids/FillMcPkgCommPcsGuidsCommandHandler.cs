5641c2672cbb5392ee08dc2be39896f30dfb43c5
TWOLD <TWOLD@equinor.com>
2023-11-29 13:15:34 +0100

    Syncronization

---
+++ src/Equinor.ProCoSys.IPO.Command/McPkgCommands/FillMcPkgCommPcsGuids/FillMcPkgCommPcsGuidsCommandHandler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Common.Misc;
+using Equinor.ProCoSys.IPO.Domain;
+using Equinor.ProCoSys.IPO.Domain.AggregateModels.ProjectAggregate;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using ServiceResult;
+using Equinor.ProCoSys.IPO.Domain.AggregateModels.InvitationAggregate;
+using System.Collections.Generic;
+using Equinor.ProCoSys.IPO.ForeignApi.MainApi.McPkg;
+
+namespace Equinor.ProCoSys.IPO.Command.McPkgCommands.FillMcPkgCommPcsGuids
+{
+    public class FillPCSGuidsCommandHandler : IRequestHandler<FillMcPkgCommPkgPCSGuidsCommand, Result<Unit>>
+    {
+        private readonly ILogger<FillMcPkgCommPkgPCSGuidsCommand> _logger;
+        private readonly IInvitationRepository _invitationRepository;
+        private readonly IMcPkgApiService _mcPkgApiService;
+        private readonly IProjectRepository _projectRepository;
+        private readonly IPlantProvider _plantProvider;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FillPCSGuidsCommandHandler(
+            ILogger<FillMcPkgCommPkgPCSGuidsCommand> logger,
+            IPlantProvider plantProvider,
+            IInvitationRepository invitationRepository,
+            IMcPkgApiService mcPkgApiService,
+            IProjectRepository projectRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _plantProvider = plantProvider;
+            _invitationRepository = invitationRepository;
+            _mcPkgApiService= mcPkgApiService;
+            _projectRepository = projectRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result<Unit>> Handle(FillMcPkgCommPkgPCSGuidsCommand request, CancellationToken cancellationToken)
+        {
+            var allMcPkgs = _invitationRepository.GetMcPkgsOnly();
+            var count = 0;
+            foreach (var mcPkg in allMcPkgs)
+            {
+                if (mcPkg.CommPkgGuid == Guid.Empty)
+                {
+
+                    var project = await _projectRepository.GetByIdAsync(mcPkg.ProjectId);
+                    IList<string> mcPkgNo = new List<string>() { mcPkg.McPkgNo};
+
+                    var mcPkgResult = await _mcPkgApiService.GetMcPkgsByMcPkgNosAsync(_plantProvider.Plant, project.Name, mcPkgNo);
+
+                    if (mcPkgResult != null && mcPkgResult.Count == 1)
+                    {
+                        mcPkg.CommPkgGuid = mcPkgResult.First().CommPkgGuid;
+                       _logger.LogInformation($"FillMcPkgCommPkgPCSGuidsCommand: McPkg {mcPkg.McPkgNo} updated with CommPkgGuid: {mcPkg.CommPkgGuid}");
+                       count++;
+                    }
+                }
+            }
+
+            if (request.SaveChanges && count > 0)
+            {
+                await _unitOfWork.SaveChangesAsync(cancellationToken);
+                _logger.LogInformation($"FillMcPkgCommPkgPCSGuidsCommand: {count} McPks updated");
+            }
+            return new SuccessResult<Unit>(Unit.Value);
+        }
+    }
+}
--- src/Equinor.ProCoSys.IPO.WebApi/Synchronization/SynchronizationService.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Synchronization/SynchronizationService.cs
@@ -8,15 +8,13 @@ using Equinor.ProCoSys.Auth.Caches;
 using Equinor.ProCoSys.Auth.Misc;
 using Equinor.ProCoSys.Common.Misc;
 using Equinor.ProCoSys.Common.Time;
-using Equinor.ProCoSys.IPO.Command.CommPkgCommands.FillCommPkgPcsGuids;
-using Equinor.ProCoSys.IPO.Command.ProjectCommands.FillProjectPcsGuids;
-using Equinor.ProCoSys.IPO.Command.McPkgCommands.FillMcPkgPcsGuids;
 using Equinor.ProCoSys.IPO.Domain.AggregateModels.SettingAggregate;
 using Equinor.ProCoSys.IPO.WebApi.Authentication;
 using MediatR;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Equinor.ProCoSys.IPO.Command.McPkgCommands.FillMcPkgCommPcsGuids;
 
 namespace Equinor.ProCoSys.IPO.WebApi.Synchronization
 {
@@ -93,10 +91,7 @@ namespace Equinor.ProCoSys.IPO.WebApi.Synchronization
                     await _claimsTransformation.TransformAsync(currentUser);
 
                     var startTime = TimeService.UtcNow;
-
-                    await _mediator.Send(new FillCommPkgPCSGuidsCommand(saveChanges?.Value == "true"));
-                    await _mediator.Send(new FillMcPkgPCSGuidsCommand(saveChanges?.Value == "true"));
-                    await _mediator.Send(new FillProjectPCSGuidsCommand(saveChanges?.Value == "true"));
+                    await _mediator.Send(new FillMcPkgCommPkgPCSGuidsCommand(saveChanges?.Value == "true"));
 
                     var endTime = TimeService.UtcNow;
 
