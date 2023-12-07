3ab30bf0690465760da9c3e6fe14ba3724be238b
Trond Lindanger <tklin@equinor.com>
2023-12-07 16:00:27 +0100

    Complete leader elector URL placed in yml.

--- src/Equinor.ProCoSys.IPO.WebApi/Startup.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Startup.cs
@@ -183,7 +183,7 @@ namespace Equinor.ProCoSys.IPO.WebApi
                 // Env variable used in kubernetes. Configuration is added for easier use locally
                 // Url will be validated during startup of service bus integration and give a
                 // Uri exception if invalid.
-                var leaderElectorUrl = "http://" + (Environment.GetEnvironmentVariable("LEADERELECTOR_SERVICE") ?? Configuration["ServiceBus:LeaderElectorUrl"]) + ":3003";
+                var leaderElectorUrl = Environment.GetEnvironmentVariable("LEADERELECTOR_SERVICE") ?? (Configuration["ServiceBus:LeaderElectorUrl"]);
 
                 services.AddPcsServiceBusIntegration(options => options
                     .UseBusConnection(Configuration.GetConnectionString("ServiceBus"))
