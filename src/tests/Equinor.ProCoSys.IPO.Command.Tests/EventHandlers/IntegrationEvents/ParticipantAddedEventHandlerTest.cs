782503a2aa880c04d65635cf1b5889ad2e305d1c
Jostein Søiland <jsoi@equinor.com>
2024-08-12 14:37:00 +0200

    Add unit test for participant type functionalrole

--- src/tests/Equinor.ProCoSys.IPO.Command.Tests/EventHandlers/IntegrationEvents/ParticipantAddedEventHandlerTest.cs
+++ src/tests/Equinor.ProCoSys.IPO.Command.Tests/EventHandlers/IntegrationEvents/ParticipantAddedEventHandlerTest.cs
@@ -66,5 +66,24 @@ namespace Equinor.ProCoSys.IPO.Command.Tests.EventHandlers.IntegrationEvents
             _integrationEventPublisherMock
                 .Verify(x => x.PublishAsync(It.IsAny<IParticipantEventV1>(), It.IsAny<CancellationToken>()), Times.Once);
         }
+
+        [TestMethod]
+        public async Task Handle_ShouldPublishWhenParticipantIsOfTypeFunctionalRole()
+        {
+            // Arrange
+            var sourceGuid = Guid.NewGuid();
+            var plant = "TestPlant";
+            var functionalRole = "A functional role";
+
+            var participant = new Participant(plant, Organization.Commissioning, IpoParticipantType.FunctionalRole,
+                functionalRole, null, null, null, null, null, 1);
+
+            // Act
+            await _dut.Handle(new ParticipantAddedEvent(plant, sourceGuid, null, participant), default);
+
+            // Assert
+            _integrationEventPublisherMock
+                .Verify(x => x.PublishAsync(It.IsAny<IParticipantEventV1>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
     }
 }
