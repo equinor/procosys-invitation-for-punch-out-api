ad85af1ff9fdcdfa15570f9b806df7f42c0bfd5c
Trond Lindanger <tklin@equinor.com>
2024-08-14 11:09:20 +0200

    99774: Included library.

--- src/Equinor.ProCoSys.IPO.Domain/AggregateModels/InvitationAggregate/IInvitationRepository.cs
+++ src/Equinor.ProCoSys.IPO.Domain/AggregateModels/InvitationAggregate/IInvitationRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Equinor.ProCoSys.Common;
 
 namespace Equinor.ProCoSys.IPO.Domain.AggregateModels.InvitationAggregate
