b5fb07f4bf4ba1c884dabcf392a705e437984f1b
Jostein Søiland <jsoi@equinor.com>
2024-08-20 08:39:48 +0200

    Add more logging

--- src/Equinor.ProCoSys.IPO.Command/EventHandlers/IntegrationEvents/CreateEventHelper.cs
+++ src/Equinor.ProCoSys.IPO.Command/EventHandlers/IntegrationEvents/CreateEventHelper.cs
@@ -6,6 +6,8 @@ using Equinor.ProCoSys.IPO.Domain.AggregateModels.ProjectAggregate;
 using Equinor.ProCoSys.IPO.MessageContracts;
 using JetBrains.Annotations;
 using MediatR;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
 
 namespace Equinor.ProCoSys.IPO.Command.EventHandlers.IntegrationEvents;
 
@@ -13,11 +15,13 @@ public class CreateEventHelper : ICreateEventHelper
 {
     private readonly IProjectRepository _projectRepository;
     private readonly IPersonRepository _personRepository;
+    private readonly ILogger<CreateEventHelper> _logger;
 
-    public CreateEventHelper(IProjectRepository projectRepository, IPersonRepository personRepository)
+    public CreateEventHelper(IProjectRepository projectRepository, IPersonRepository personRepository, ILogger<CreateEventHelper> logger)
     {
         _projectRepository = projectRepository;
         _personRepository = personRepository;
+        _logger = logger;
     }
 
     public async Task<IInvitationEventV1> CreateInvitationEvent(Invitation invitation)
@@ -56,12 +60,15 @@ public class CreateEventHelper : ICreateEventHelper
     [ItemCanBeNull]
     public async Task<IParticipantEventV1> CreateParticipantEvent(Participant participant, Invitation invitation)
     {
+        _logger.LogInformation($"Debug: Participant: FunctionalRoleCode: {participant.FunctionalRoleCode}, LastName: {participant.LastName}, Type {participant.Type.ToString()} ");
         //Only export functional roles and persons of type person, filter out participants who are added as members of a functional role
         if (!FunctionalRoleOrPersonAsPersonType(participant))
         {
+            _logger.LogInformation("Debug: NOT a functional role or person as persontype");
             return null;
         }
-        
+        _logger.LogInformation("Debug: It IS a functional role or person as persontype");
+
         var project = await _projectRepository.GetByIdAsync(invitation.ProjectId);
 
         var signedBy = participant.SignedBy.HasValue
