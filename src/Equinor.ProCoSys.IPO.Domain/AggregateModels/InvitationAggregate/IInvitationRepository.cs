c059fbc3b37cd79440310940c8a50564f60e0eb6
trondlindanger <66942962+trondlindanger@users.noreply.github.com>
2024-08-22 13:08:30 +0200

    Feature/99774 rewrite service bus receiver from using old values to using procosys guid (#313)
    * 99774: Handling Guid's in bus event for comm pkgs.
    
    * 99774: Handling Guid's in bus events for comm pkgs.
    
    * 99774: Minor adjustments related to code review.
    
    * 99774: Included library.
    
    * 99774: Simplifying identification of mcpkgs to move.
    
    * 99774: Moving comment to top of class.

--- src/Equinor.ProCoSys.IPO.Domain/AggregateModels/InvitationAggregate/IInvitationRepository.cs
+++ src/Equinor.ProCoSys.IPO.Domain/AggregateModels/InvitationAggregate/IInvitationRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Equinor.ProCoSys.Common;
 
 namespace Equinor.ProCoSys.IPO.Domain.AggregateModels.InvitationAggregate
@@ -6,13 +7,15 @@ namespace Equinor.ProCoSys.IPO.Domain.AggregateModels.InvitationAggregate
     public interface IInvitationRepository : IRepository<Invitation>
     {
         void UpdateProjectOnInvitations(string projectName, string description);
-        void UpdateCommPkgOnInvitations(string projectName, string commPkgNo, string description);
+        void UpdateCommPkgOnInvitations(Guid commPkgGuid, string description);
         void UpdateMcPkgOnInvitations(string projectName, string mcPkgNo, string description);
         void UpdateFunctionalRoleCodesOnInvitations(string plant, string functionalRoleCodeOld, string functionalRoleCodeNew);
         void RemoveParticipant(Participant participant);
         void RemoveAttachment(Attachment attachment);
         void RemoveInvitation(Invitation invitation);
-        void MoveCommPkg(string fromProject, string toProject, string commPkgNo, string description);
+        bool ShouldMoveCommPkg(Guid toProjectGuid, Guid commPkgGuid);
+
+        void MoveCommPkg(Guid toProjectGuid, Guid commPkgGuid, string description);
         void MoveMcPkg(string projectName, string fromCommPkgNo, string toCommPkgNo, string fromMcPkgNo, string toMcPkgNo, string description);
         void RfocAcceptedHandling(string projectName, IList<string> commPkgNosWithAcceptedRfoc, IList<string> mcPkgNosWithAcceptedRfoc);
         void RfocVoidedHandling(string projectName, IList<string> commPkgNos, IList<string> mcPkgNos);
