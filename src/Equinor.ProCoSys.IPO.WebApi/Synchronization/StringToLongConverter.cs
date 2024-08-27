60d71ea8e2fdafd5c7dbc5ebcceff4ac876bb136
Trond Lindanger <tklin@equinor.com>
2024-08-27 09:49:44 +0200

    115669: CommPkgId is not converted from string to long during deserialization

---
+++ src/Equinor.ProCoSys.IPO.WebApi/Synchronization/StringToLongConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Equinor.ProCoSys.IPO.WebApi.Synchronization;
+
+public class StringToLongConverter : JsonConverter<long>
+{
+    public override long Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.String && long.TryParse(reader.GetString(), out long result))
+        {
+            return result;
+        }
+        throw new JsonException($"Unable to deserialize JSON to CommPkgEvent. Cannot convert {reader.GetString()} to {typeof(long)}.");
+    }
+
+    public override void Write(Utf8JsonWriter writer, long value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
+    }
+}
