6b14ced2931f31f0db5e2add2e57e9a6de58a002
Trond Lindanger <tklin@equinor.com>
2024-08-27 15:34:45 +0200

    115677: Include all type mapping for deserialization of commPkgEvent

--- src/Equinor.ProCoSys.IPO.WebApi/Synchronization/CommPkgEvent.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Synchronization/CommPkgEvent.cs
@@ -14,12 +14,14 @@ namespace Equinor.ProCoSys.IPO.WebApi.Synchronization
         public long CommPkgId { get; init; }
         public string CommPkgNo { get; init; }
         public string CommPkgStatus { get; init; }
+        [JsonConverter(typeof(StringToDateTimeConverter))]
         public DateTime CreatedAt { get; init; }
         public string? DCCommPkgStatus { get; init; }
         public bool? Demolition { get; init; }
         public string Description { get; init; }
         public string? DescriptionOfWork { get; init; }
         public bool IsVoided { get; init; }
+        [JsonConverter(typeof(StringToDateTimeConverter))]
         public DateTime LastUpdated { get; init; }
         public string? Phase { get; init; }
         public string Plant { get; init; }
