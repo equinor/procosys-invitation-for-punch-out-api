904a00af69bb30290234e0dc30b8777f29c3b15b
Trond Lindanger <tklin@equinor.com>
2024-08-28 13:14:37 +0200

    115677: Json convert for string to guid type.

---
+++ src/Equinor.ProCoSys.IPO.WebApi/Synchronization/StringToGuidConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Equinor.ProCoSys.IPO.WebApi.Synchronization;
+
+public class StringToGuidConverter : JsonConverter<Guid>
+{
+    public override Guid Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.String)
+        {
+            var stringGuid = reader.GetString();
+            if (Guid.TryParseExact(stringGuid, "N", out Guid guid)) // "N" format: 32 digits without hyphens
+            {
+                return guid;
+            }
+            else
+            {
+                throw new JsonException($"The JSON value is not in a guid format: {stringGuid}");
+            }
+        }
+
+        throw new JsonException("The JSON value could not be read as a guid.");
+    }
+
+    public override void Write(Utf8JsonWriter writer, Guid value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString("N")); // "N" format: 32 digits without hyphens
+    }
+}
---
+++ src/tests/Equinor.ProCoSys.IPO.WebApi.Tests/Synchronization/StringToGuidConverterTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Equinor.ProCoSys.IPO.WebApi.Synchronization;
+
+namespace Equinor.ProCoSys.IPO.WebApi.Tests.Synchronization
+{
+
+    [TestClass]
+    public class StringToGuidConverterTests
+    {
+        private JsonSerializerOptions _options;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _options = new JsonSerializerOptions
+            {
+                Converters = { new StringToGuidConverter() }
+            };
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(JsonException))]
+        public void Read_WithInvalidString_ThrowsJsonException()
+        {
+            // Arrange
+            var input = "\"not_a_valid_guid\"";
+
+            // Act
+            var ex = Assert.ThrowsException<JsonException>(() => JsonSerializer.Deserialize<Guid>(input, _options));
+
+            // Assert
+            StringAssert.Contains(ex.Message, "The JSON value is not in a guid format:");
+        }
+
+        [TestMethod]
+        public void Read_WithValidString_ReturnsGuidValue()
+        {
+            // Arrange
+            var expectedGuid = new Guid("20964082-0381-4F9C-E063-0A14000A02B8"); // With hyphens for Guid parsing
+            var input = "\"2096408203814F9CE0630A14000A02B8\""; // Without hyphens in JSON
+
+            // Act
+            var resultGuid = JsonSerializer.Deserialize<Guid>(input, _options);
+
+            // Assert
+            Assert.AreEqual(expectedGuid, resultGuid);
+        }
+    }
+}
