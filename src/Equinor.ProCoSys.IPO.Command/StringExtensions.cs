6c75512af83577c59097d5350e601ef14587941c
Jan Inge Dalsbø <JDALSB@equinor.com>
2025-02-25 07:36:53 +0100

    Use string extension method to strip html

---
+++ src/Equinor.ProCoSys.IPO.Command/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Equinor.ProCoSys.IPO.Command;
+
+public static class StringExtensions
+{
+    public static string StripHtml(this string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return string.Empty;
+        }
+
+        return Regex.Replace(input, "<.*?>", string.Empty);
+    }
+}
---
+++ src/tests/Equinor.ProCoSys.IPO.Command.Tests/StringExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.ProCoSys.IPO.Command.Tests;
+
+[TestClass]
+public class StringExtensionsTests
+{
+    [TestMethod]
+    [DataRow("<p>This is a paragraph.</p>", "This is a paragraph.")]
+    [DataRow("<div><p>Nested tags</p></div>", "Nested tags")]
+    [DataRow("<a href='https://example.com'>Link</a>", "Link")]
+    [DataRow("<b>Bold</b> and <i>Italic</i>", "Bold and Italic")]
+    [DataRow("", "")]
+    [DataRow(null, "")]
+    [DataRow("No HTML tags", "No HTML tags")]
+    public void StripHtml_RemovesHtmlTags(string input, string expected)
+    {
+        // Act
+        var result = input.StripHtml();
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+}
