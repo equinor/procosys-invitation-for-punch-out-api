422c77ae7df32f72c581420cec37a65c654c7885
Richard Dungal <richard.dungal@bouvet.no>
2025-08-11 15:41:44 +0200

    initial

--- src/Equinor.ProCoSys.IPO.WebApi/Program.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Program.cs
@@ -1,56 +1,299 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json.Serialization;
 using Azure.Identity;
+using Equinor.ProCoSys.Auth;
+using Equinor.ProCoSys.Common.Misc;
+using Equinor.ProCoSys.Common.Swagger;
+using Equinor.ProCoSys.IPO.Command;
+using Equinor.ProCoSys.IPO.Query;
+using Equinor.ProCoSys.IPO.WebApi.DIModules;
+using Equinor.ProCoSys.IPO.WebApi.Middleware;
+using Equinor.ProCoSys.IPO.WebApi.Seeding;
+using Equinor.ProCoSys.IPO.WebApi.Synchronization;
+using Equinor.ProCoSys.PcsServiceBus;
+using Equinor.ProCoSys.PcsServiceBus.Sender.Interfaces;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
-namespace Equinor.ProCoSys.IPO.WebApi
+// Top-level statements start here
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var environment = builder.Environment;
+
+// Configure Azure App Configuration if enabled
+if (configuration.GetValue<bool>("UseAzureAppConfiguration"))
 {
-    public class Program
+    builder.Configuration.AddAzureAppConfiguration(options =>
     {
-        public static void Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            host.Run();
-        }
+        var connectionString = configuration["ConnectionStrings:AppConfig"];
+        options.Connect(connectionString)
+            .ConfigureKeyVault(kv =>
+            {
+                kv.SetCredential(new ManagedIdentityCredential());
+                // Use DefaultAzureCredential in local dev env.
+                // kv.SetCredential(new DefaultAzureCredential());
+            })
+            .Select(KeyFilter.Any)
+            .Select(KeyFilter.Any, environment.EnvironmentName)
+            .ConfigureRefresh(refreshOptions =>
+            {
+                refreshOptions.Register("Sentinel", true);
+                refreshOptions.SetCacheExpiration(TimeSpan.FromSeconds(30));
+            });
+    });
+}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    var settings = config.Build();
-                    var azConfig = settings.GetValue<bool>("UseAzureAppConfiguration");
-                    if (azConfig)
-                    {
-                        config.AddAzureAppConfiguration(options =>
-                        {
-                            var connectionString = settings["ConnectionStrings:AppConfig"];
-                            options.Connect(connectionString)
-                                .ConfigureKeyVault(kv =>
-                                {
-                                    kv.SetCredential(new ManagedIdentityCredential());
-                                    // Use DefaultAzureCredential in local dev env.
-                                    // kv.SetCredential(new DefaultAzureCredential());
-                                })
-                                .Select(KeyFilter.Any)
-                                .Select(KeyFilter.Any, context.HostingEnvironment.EnvironmentName)
-                                .ConfigureRefresh(refreshOptions =>
-                                {
-                                    refreshOptions.Register("Sentinel", true);
-                                    refreshOptions.SetCacheExpiration(TimeSpan.FromSeconds(30));
-                                });
-                        });
-                    }
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseKestrel(options =>
-                    {
-                        options.AddServerHeader = false;
-                        options.Limits.MaxRequestBodySize = null;
-                    });
-                    webBuilder.UseStartup<Startup>();
-                });
+// Configure server options
+builder.WebHost.UseKestrel(options =>
+{
+    options.AddServerHeader = false;
+    options.Limits.MaxRequestBodySize = null;
+});
+
+// Add services to the container
+if (environment.IsDevelopment() || environment.IsEnvironment("Test"))
+{
+    var migrateDatabase = configuration.GetValue<bool>("MigrateDatabase");
+    if (migrateDatabase)
+    {
+        builder.Services.AddHostedService<DatabaseMigrator>();
     }
 }
+
+if (environment.IsDevelopment())
+{
+    DebugOptions.DebugEntityFrameworkInDevelopment = configuration.GetValue<bool>("DebugEntityFrameworkInDevelopment");
+
+    if (configuration.GetValue<bool>("SeedDummyData"))
+    {
+        builder.Services.AddHostedService<Seeder>();
+    }
+}
+
+// Add authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            configuration.Bind("API", options);
+        });
+
+// Configure CORS
+const string AllowAllOriginsCorsPolicy = "AllowAllOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(AllowAllOriginsCorsPolicy,
+        builder =>
+        {
+            builder
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+
+// Configure controllers and JSON options
+builder.Services.AddMvc(config =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    config.Filters.Add(new AuthorizeFilter(policy));
+}).AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+if (configuration.GetValue<bool>("UseAzureAppConfiguration"))
+{
+    builder.Services.AddAzureAppConfiguration();
+}
+
+// Add validation
+builder.Services.AddFluentValidationAutoValidation(fv =>
+{
+    fv.DisableDataAnnotationsValidation = true;
+});
+builder.Services.AddValidatorsFromAssemblies(new List<Assembly>
+{
+    typeof(IQueryMarker).GetTypeInfo().Assembly,
+    typeof(ICommandMarker).GetTypeInfo().Assembly,
+    typeof(Program).Assembly
+});
+
+// Configure Swagger
+var scopes = configuration.GetSection("Swagger:Scopes")?.Get<Dictionary<string, string>>() ?? new Dictionary<string, string>();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProCoSys IPO API", Version = "v1" });
+
+    //Define the OAuth2.0 scheme that's in use (i.e. Implicit Flow)
+    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            Implicit = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri(configuration["Swagger:AuthorizationUrl"]),
+                Scopes = scopes
+            }
+        }
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+            },
+            scopes.Keys.ToArray()
+        }
+    });
+
+    c.OperationFilter<AddRoleDocumentation>();
+});
+
+builder.Services.ConfigureSwaggerGen(options =>
+{
+    options.CustomSchemaIds(x => x.FullName);
+});
+
+builder.Services.AddFluentValidationRulesToSwagger();
+
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+});
+
+builder.Services.AddPcsAuthIntegration();
+
+if (!environment.IsEnvironment("IntegrationTest"))
+{
+    builder.Services.AddFusionIntegration(options =>
+    {
+        options.UseServiceInformation("PCS IPO", environment.EnvironmentName); // Environment identifier
+        options.UseDefaultEndpointResolver(
+            configuration
+                ["Meetings:Environment"]); // Fusion environment "fprd" = prod, "fqa" = qa, "ci" = dev/test etc
+        options.UseDefaultTokenProvider(opts =>
+        {
+            opts.ClientId = configuration["Meetings:ClientId"]; // Application client ID
+            opts.ClientSecret = configuration["Meetings:ClientSecret"]; // Application client secret
+        });
+        options.AddMeetings(s => s.SetHttpClientTimeout(
+            TimeSpan.FromSeconds(configuration.GetValue<double>("FusionRequestTimeout")),
+            TimeSpan.FromSeconds(configuration.GetValue<double>("FusionTotalTimeout"))));
+        options.DisableClaimsTransformation(); // Disable this - Fusion adds relevant claims
+    });
+}
+
+builder.Services.AddApplicationInsightsTelemetry(options =>
+{
+    options.ConnectionString = configuration["ApplicationInsights:ConnectionString"];
+});
+
+builder.Services.AddMediatrModules();
+builder.Services.AddApplicationModules(configuration);
+
+var serviceBusEnabled = configuration.GetValue<bool>("ServiceBus:Enable") &&
+    (!environment.IsDevelopment() || configuration.GetValue<bool>("ServiceBus:EnableInDevelopment"));
+if (serviceBusEnabled)
+{
+    // Env variable used in kubernetes. Configuration is added for easier use locally
+    // Url will be validated during startup of service bus integration and give a
+    // Uri exception if invalid.
+    var leaderElectorUrl = Environment.GetEnvironmentVariable("LEADERELECTOR_SERVICE") ?? (configuration["ServiceBus:LeaderElectorUrl"]);
+
+    builder.Services.AddPcsServiceBusIntegration(options => options
+        .UseBusConnection(configuration.GetConnectionString("ServiceBus"))
+        .WithLeaderElector(leaderElectorUrl)
+        .WithRenewLeaseInterval(int.Parse(configuration["ServiceBus:LeaderElectorRenewLeaseInterval"]))
+        .WithSubscription(PcsTopicConstants.Ipo, "ipo_ipo")
+        .WithSubscription(PcsTopicConstants.Project, "ipo_project")
+        .WithSubscription(PcsTopicConstants.CommPkg, "ipo_commpkg")
+        .WithSubscription(PcsTopicConstants.McPkg, "ipo_mcpkg")
+        .WithSubscription(PcsTopicConstants.Library, "ipo_library")
+        .WithSubscription(PcsTopicConstants.Certificate, "ipo_certificate")
+        //THIS METHOD SHOULD BE FALSE IN NORMAL OPERATION.
+        //ONLY SET TO TRUE WHEN A LARGE NUMBER OF MESSAGES HAVE FAILED AND ARE COPIED TO DEAD LETTER.
+        //WHEN SET TO TRUE, MESSAGES ARE READ FROM DEAD LETTER QUEUE INSTEAD OF NORMAL QUEUE
+        .WithReadFromDeadLetterQueue(configuration.GetValue("ServiceBus:ReadFromDeadLetterQueue", defaultValue: false)));
+
+    var topics = configuration["ServiceBus:TopicNames"];
+    builder.Services.AddTopicClients(configuration.GetConnectionString("ServiceBus"), topics);
+}
+else
+{
+    builder.Services.AddSingleton<IPcsBusSender>(new DisabledServiceBusSender());
+}
+
+builder.Services.AddHostedService<VerifyApplicationExistsAsPerson>();
+
+// Build the application
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (configuration.GetValue<bool>("UseAzureAppConfiguration"))
+{
+    app.UseAzureAppConfiguration();
+}
+
+if (environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseGlobalExceptionHandling();
+
+app.UseCors(AllowAllOriginsCorsPolicy);
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProCoSys IPO API V1");
+    c.DocExpansion(DocExpansion.List);
+    c.DisplayRequestDuration();
+
+    c.OAuthClientId(configuration["Swagger:ClientId"]);
+    c.OAuthAppName("ProCoSys IPO API V1");
+    c.OAuthScopeSeparator(" ");
+    c.OAuthAdditionalQueryStringParams(new Dictionary<string, string> { { "resource", configuration["API:Audience"] } });
+});
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+// Order of adding middlewares is crucial. Some depend that other has been run in advance
+app.UseCurrentPlant();
+app.UseCurrentBearerToken();
+app.UseAuthentication();
+app.UseCurrentUser();
+app.UsePersonValidator();
+app.UsePlantValidator();
+app.UseVerifyOidInDb();
+app.UseAuthorization();
+
+app.UseResponseCompression();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+// Run the application
+app.Run();
