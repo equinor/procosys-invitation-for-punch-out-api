80bad693cd7e945b07f5f55c7636e16c28582051
Richard Dungal <richard.dungal@bouvet.no>
2025-08-15 14:29:17 +0200

    working

--- src/Equinor.ProCoSys.IPO.ServiceBusProcessor/Program.cs
+++ src/Equinor.ProCoSys.IPO.ServiceBusProcessor/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder();
+// builder.Configuration.AddJsonFile("appsettings.json");
 builder.Configuration.AddJsonFile("appsettings.local.json", optional: true);
 builder.Services.AddOptions<TopicSubscriptionOptions>().BindConfiguration(nameof(TopicSubscriptionOptions));
 builder.Services.AddTopicSubscriptionServices();
