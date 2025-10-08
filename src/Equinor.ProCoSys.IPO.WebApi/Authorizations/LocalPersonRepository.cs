8a595b3fe371c4fbb9e7da884f97e52bb231dd22
Richard Dungal <richard.dungal@bouvet.no>
2025-10-08 14:20:45 +0200

    LocalPersonRepository

--- src/Equinor.ProCoSys.IPO.WebApi/Authorizations/LocalPersonRepository.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Authorizations/LocalPersonRepository.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Equinor.ProCoSys.Auth.Authorization;
+using Equinor.ProCoSys.Auth.Person;
 using Equinor.ProCoSys.Common;
 using Equinor.ProCoSys.IPO.Domain.AggregateModels.PersonAggregate;
 using Microsoft.EntityFrameworkCore;
@@ -14,12 +16,23 @@ namespace Equinor.ProCoSys.IPO.WebApi.Authorizations
 
         public LocalPersonRepository(IReadOnlyContext context) => _context = context;
 
-        public async Task<bool> ExistsAsync(Guid userOid)
+        public async Task<bool> ExistsAsync(Guid userOid, CancellationToken cancellationToken)
         {
             var exists = await (from person in _context.QuerySet<Person>()
                                 where person.Guid == userOid
-                                select person).AnyAsync();
+                                select person).AnyAsync(cancellationToken);
             return exists;
         }
+        
+        public async Task<ProCoSysPerson> GetAsync(Guid userOid, CancellationToken cancellationToken)
+            => await (from person in _context.QuerySet<Person>()
+                where person.Guid == userOid
+                select new ProCoSysPerson
+                {
+                    AzureOid = person.Guid.ToString(),
+                    Email = person.Email,
+                    FirstName = person.FirstName,
+                    LastName = person.LastName,
+                }).SingleOrDefaultAsync(cancellationToken);
     }
 }
