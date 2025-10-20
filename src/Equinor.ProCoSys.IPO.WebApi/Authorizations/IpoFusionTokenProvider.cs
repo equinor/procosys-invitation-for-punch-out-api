0f5bdbf28360ac4efae42609e00dd51ce85287d2
Richard Dungal <richard.dungal@bouvet.no>
2025-10-20 17:47:15 +0200

    primary

--- src/Equinor.ProCoSys.IPO.WebApi/Authorizations/IpoFusionTokenProvider.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Authorizations/IpoFusionTokenProvider.cs
@@ -1,52 +1,37 @@
 using System;
 using System.Threading.Tasks;
 using Fusion.Integration.Configuration;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Identity.Web;
 
 namespace Equinor.ProCoSys.IPO.WebApi.Authorizations;
 
-public class IpoFusionTokenProvider : IFusionTokenProvider
+public class IpoFusionTokenProvider(ITokenAcquisition tokenAcquisition) : IFusionTokenProvider
 {
-    private readonly ITokenAcquisition _tokenAcquisition;
-    private readonly string _scope;
-
-    public IpoFusionTokenProvider(ITokenAcquisition tokenAcquisition, IConfiguration config)
-    {
-        _tokenAcquisition = tokenAcquisition;
-
-        var azureAdConfig = config.GetSection("AzureAd");
-        var clientId = azureAdConfig.GetValue<string>("ClientId");
-        
-        _scope = $"{clientId}/.default";
-        
-    }
-
     public async Task<string> GetApplicationTokenAsync(string scope)
     {
-        if (TryConvertV1ScopeTov2ScopeString(scope, out var scopeV2))
+        if (TryConvertV1ScopeTov2(scope, out var scopeV2))
         {
             scope = scopeV2;
         }
         
-        return await _tokenAcquisition.GetAccessTokenForAppAsync(
+        return await tokenAcquisition.GetAccessTokenForAppAsync(
             scope,
             tokenAcquisitionOptions: new TokenAcquisitionOptions());
     }
 
     public async Task<string> GetDelegatedToken(string scope)
     {
-        if (TryConvertV1ScopeTov2ScopeString(scope, out var scopeV2))
+        if (TryConvertV1ScopeTov2(scope, out var scopeV2))
         {
             scope = scopeV2;
         }
         
-        return await _tokenAcquisition.GetAccessTokenForUserAsync(
+        return await tokenAcquisition.GetAccessTokenForUserAsync(
             [scope],
             tokenAcquisitionOptions: new TokenAcquisitionOptions());
     }
 
-    private static bool TryConvertV1ScopeTov2ScopeString(string v1ScopeString, out string v2ScopeString)
+    private static bool TryConvertV1ScopeTov2(string v1ScopeString, out string v2ScopeString)
     {
         // Logic is based on DefaultFusionTokenProvider GetDefaultScope function.
         // https://github.com/equinor/fusion-integration-lib/blob/3e44d2899c8b6563f1cf8bdb132dd905b35f7df3/src/Fusion.Integration/Internals/DefaultFusionTokenProvider.cs#L154
