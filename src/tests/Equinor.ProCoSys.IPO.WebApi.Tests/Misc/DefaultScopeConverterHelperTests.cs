54e35bfc276d869868c649cb6949bce065adace9
Richard Dungal <richard.dungal@bouvet.no>
2025-10-21 09:33:49 +0200

    r

--- src/tests/Equinor.ProCoSys.IPO.WebApi.Tests/Misc/DefaultScopeConverterHelperTests.cs
+++ src/tests/Equinor.ProCoSys.IPO.WebApi.Tests/Misc/DefaultScopeConverterHelperTests.cs
@@ -47,7 +47,7 @@ public class DefaultScopeConverterHelperTests
     }
     
     [DataTestMethod]
-    [DataRow("123e4567-e89b-12d3-a456-426614174000")]
+    [DataRow("123e4567-e89b-12d3-a456-426614174000/.default", "123e4567-e89b-12d3-a456-426614174000")]
     public void TryConvertToDefaultScope_ShouldReturn_ExpectedDefaultScope_WhenProvidedScopeIs(string expected, string scope)
     {
         // Act
