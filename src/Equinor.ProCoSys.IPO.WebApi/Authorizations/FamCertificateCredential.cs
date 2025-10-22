83d074afd181be528a3cb7e90eafa3eed4b3284b
Richard Dungal <richard.dungal@bouvet.no>
2025-10-22 17:54:23 +0200

    BaseCertificateCredential

--- src/Equinor.ProCoSys.IPO.WebApi/Authorizations/FamCertificateCredential.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Authorizations/FamCertificateCredential.cs
@@ -8,46 +8,10 @@ using Microsoft.Extensions.Configuration;
 
 namespace Equinor.ProCoSys.IPO.WebApi.Authorizations;
 
-public class FamCertificateCredential : IFamCredential
-{
-    private readonly string _clientId;
-    private readonly string _keyVaultCertificateName;
-    private readonly Uri _keyVaultUri;
-    private readonly string _tenantId;
-
-    public FamCertificateCredential(IConfiguration config)
-    {
-        var commonLibConfig = config.GetSection("CommonLibConfig");
-        _clientId = commonLibConfig.GetValue<string>("ClientId");
-
-        var credentialConfig = commonLibConfig.GetSection("ClientCredentials:0");
-        _keyVaultCertificateName = credentialConfig.GetValue<string>("KeyVaultCertificateName");
-
-        var keyVaultUrl = credentialConfig.GetValue<string>("KeyVaultUrl");
-        _keyVaultUri = new Uri(keyVaultUrl!);
-
-        var azureAdConfig = config.GetSection("AzureAd");
-        _tenantId = azureAdConfig.GetValue<string>("TenantId");
-    }
-
-    public TokenCredential GetToken()
-    {
-        var secret = RetrieveSecret();
-        var certificate = ParseCertificate(secret);
-
-        return new ClientCertificateCredential(_tenantId, _clientId, certificate);
-    }
-
-    private KeyVaultSecret RetrieveSecret()
-    {
-        var defaultCredential = new DefaultAzureCredential();
-        var secretClient = new SecretClient(_keyVaultUri, defaultCredential);
-        return secretClient.GetSecret(_keyVaultCertificateName).Value;
-    }
-
-    private static X509Certificate2 ParseCertificate(KeyVaultSecret secret)
-    {
-        var certificateBytes = Convert.FromBase64String(secret.Value);
-        return new X509Certificate2(certificateBytes, (string)null, X509KeyStorageFlags.MachineKeySet);
-    }
-}
+public class FamCertificateCredential(IConfiguration config) : 
+    BaseCertificateCredential(
+    config.GetValue<string>("CommonLibConfig:ClientId"),
+    config.GetValue<string>("CommonLibConfig:ClientCredentials:0:KeyVaultCertificateName"),
+    new Uri(config.GetValue<string>("CommonLibConfig:ClientCredentials:0:KeyVaultUrl")!),
+    config.GetValue<string>("AzureAd:TenantId")), 
+    IFamCredential;
