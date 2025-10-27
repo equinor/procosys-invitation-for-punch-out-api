aa7590236dc37960692a00cfd0a541fb9e1940f5
Richard Dungal <richard.dungal@bouvet.no>
2025-10-27 10:10:52 +0100

    r

--- src/Equinor.ProCoSys.IPO.WebApi/Authorizations/TokenCredentials/FamCertificateCredential.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Authorizations/TokenCredentials/FamCertificateCredential.cs
@@ -6,8 +6,8 @@ namespace Equinor.ProCoSys.IPO.WebApi.Authorizations.TokenCredentials;
 
 public class FamCertificateCredential(IConfiguration config) :
     BaseCertificateCredential(
-    config.GetValue<string>("CommonLibConfig:ClientId"),
-    config.GetValue<string>("CommonLibConfig:ClientCredentials:0:KeyVaultCertificateName"),
-    new Uri(config.GetValue<string>("CommonLibConfig:ClientCredentials:0:KeyVaultUrl")!),
+    config.GetValue<string>("Fam:ClientId"),
+    config.GetValue<string>("Fam:ClientCredentials:0:KeyVaultCertificateName"),
+    new Uri(config.GetValue<string>("Fam:ClientCredentials:0:KeyVaultUrl")!),
     config.GetValue<string>("AzureAd:TenantId")),
     IFamCredential, ITokenCredential;
