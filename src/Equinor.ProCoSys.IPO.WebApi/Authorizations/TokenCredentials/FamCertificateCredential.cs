f342218a7824bfb97075f1bf1d564182da602d9a
Richard Thor Fridriksson Dungal <54837611+richard1927@users.noreply.github.com>
2025-10-30 14:26:55 +0100

    refactor: removing duplicate common lib config (#376)

--- src/Equinor.ProCoSys.IPO.WebApi/Authorizations/TokenCredentials/FamCertificateCredential.cs
+++ src/Equinor.ProCoSys.IPO.WebApi/Authorizations/TokenCredentials/FamCertificateCredential.cs
@@ -6,8 +6,8 @@ namespace Equinor.ProCoSys.IPO.WebApi.Authorizations.TokenCredentials;
 
 public class FamCertificateCredential(IConfiguration config) :
     BaseCertificateCredential(
-    config.GetValue<string>("CommonLibConfig:ClientId"),
-    config.GetValue<string>("CommonLibConfig:ClientCredentials:0:KeyVaultCertificateName"),
-    new Uri(config.GetValue<string>("CommonLibConfig:ClientCredentials:0:KeyVaultUrl")!),
+    config.GetValue<string>("AzureAd:ClientId"),
+    config.GetValue<string>("AzureAd:ClientCredentials:0:KeyVaultCertificateName"),
+    new Uri(config.GetValue<string>("AzureAd:ClientCredentials:0:KeyVaultUrl")!),
     config.GetValue<string>("AzureAd:TenantId")),
     IFamCredential, ITokenCredential;
